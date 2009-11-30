0d9a5119af1a0c30396b8fe3b976b67a0b756244
lucantiga <lucantiga@94be75cf-be22-0410-a3f1-cbcec1c01a80>
2009-11-30 20:29:39 +0000

    Adding surface append script
    git-svn-id: https://vmtk.svn.sourceforge.net/svnroot/vmtk@233 94be75cf-be22-0410-a3f1-cbcec1c01a80

--- vmtkScripts/vmtkscripts.py
+++ vmtkScripts/vmtkscripts.py
@@ -73,6 +73,7 @@ __all__ = [
   'vmtkrenderer',
   'vmtkrendertoimage',
   'vmtkrbfinterpolation',
+  'vmtksurfaceappend',
   'vmtksurfacecapper',
   'vmtksurfacecelldatatopointdata',
   'vmtksurfacecenterlineprojection',
---
+++ vmtkScripts/vmtksurfaceappend.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+## Program:   VMTK
+## Module:    $RCSfile: vmtksurfaceappend.py,v $
+## Language:  Python
+## Date:      $Date: 2005/09/14 09:49:59 $
+## Version:   $Revision: 1.4 $
+
+##   Copyright (c) Luca Antiga, David Steinman. All rights reserved.
+##   See LICENCE file for details.
+
+##      This software is distributed WITHOUT ANY WARRANTY; without even 
+##      the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+##      PURPOSE.  See the above copyright notices for more information.
+
+import vtk
+import sys
+
+import pypes
+
+vmtksurfaceappend = 'vmtkSurfaceAppend'
+
+class vmtkSurfaceAppend(pypes.pypeScript):
+
+    def __init__(self):
+
+        pypes.pypeScript.__init__(self)
+
+        self.Surface = None
+        self.Surface2 = None
+
+        self.SetScriptName('vmtksurfaceappend')
+        self.SetScriptDoc('scale a surface by an isotropic factor')
+        self.SetInputMembers([
+            ['Surface','i','vtkPolyData',1,'','the input surface','vmtksurfacereader'],
+            ['Surface2','i2','vtkPolyData',1,'','the second input surface','vmtksurfacereader'],
+            ])
+        self.SetOutputMembers([
+            ['Surface','o','vtkPolyData',1,'','the output surface','vmtksurfacewriter']
+            ])
+
+    def Execute(self):
+
+        if (self.Surface == None):
+            self.PrintError('Error: no Surface.')
+
+        appendFilter = vtk.vtkAppendPolyData()
+        appendFilter.AddInput(self.Surface)
+        appendFilter.AddInput(self.Surface2)
+        appendFilter.Update()
+
+        self.Surface = appendFilter.GetOutput()
+
+        if self.Surface.GetSource():
+            self.Surface.GetSource().UnRegisterAllOutputs()
+
+
+if __name__=='__main__':
+    main = pypes.pypeMain()
+    main.Arguments = sys.argv
+    main.Execute()
