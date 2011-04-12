7b27e5b7e0776c595d6c698a86d9b71d53866e89
Luca Antiga <luca.antiga@orobix.com>
2011-04-12 09:22:13 +0200

    Properly initializing std::string

--- vtkVmtk/IO/vtkvmtkTetGenReader.cxx
+++ vtkVmtk/IO/vtkvmtkTetGenReader.cxx
@@ -112,7 +112,7 @@ int vtkvmtkTetGenReader::RequestData(
     }
 
   std::string line;
-  std::string delimiter = " ";
+  std::string delimiter(" ");
   std::vector<std::string> tokens;
 
   std::getline(nodeStream,line);
