b930fb8f479c7501aa320699b917a413420a6dd1
Luca Antiga <luca.antiga@orobix.com>
2012-01-23 12:32:10 +0100

    Updating references to MSVC redist files

--- distribution/scripts/getbuildvmtk.py
+++ distribution/scripts/getbuildvmtk.py
@@ -239,11 +239,11 @@ def build_vmtk():
             if on_x86():
                 if not os.path.isfile(vcredist_x86):
                     download("http://download.microsoft.com/download/d/d/9/dd9a82d0-52ef-40db-8dab-795376989c03/vcredist_x86.exe")
-                options.append('-DVCREDIST_2008_EXE:FILEPATH=' + vcredist_x86)
+                options.append('-DVCREDIST_EXE:FILEPATH=' + vcredist_x86)
             if on_x64():
                 if not os.path.isfile(vcredist_x64):
                     download("http://download.microsoft.com/download/2/d/6/2d61c766-107b-409d-8fba-c39e61ca08e8/vcredist_x64.exe")
-                options.append('-DVCREDIST_2008_EXE:FILEPATH=' + vcredist_x64)
+                options.append('-DVCREDIST_EXE:FILEPATH=' + vcredist_x64)
     options = ' '.join(options)
 
     build_cmake_project(VMTK_BUILD_DIR, source_dir, options)
