3b50ccd461bca0b633c37fdcc79d764a6e2f255f
Luca Antiga <luca.antiga@orobix.com>
2013-11-09 00:25:14 +0100

    Go with proper repository for ITK

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -46,8 +46,7 @@ if( NOT USE_SYSTEM_ITK )
   ##
   set( proj ITK )
   ExternalProject_Add( ${proj}
-    GIT_REPOSITORY "${GIT_PROTOCOL}://github.com/vmtk/ITK.git"
-    #GIT_TAG "v3.20.1-vmtk"
+    GIT_REPOSITORY "${GIT_PROTOCOL}://github.com/Kitware/ITK.git"
     SOURCE_DIR "${CMAKE_BINARY_DIR}/ITK"
     BINARY_DIR ITK-Build
     CMAKE_GENERATOR ${gen}
