bef8ce18493d25acec19b8b42574b61bf4a37ebf
daron1337 <simone.manini@gmail.com>
2014-01-15 12:53:49 +0100

    added PKG-INFO and fixed vmtk.py and vmtk_post_install

--- distribution/egg/setup.py
+++ distribution/egg/setup.py
@@ -7,8 +7,41 @@ from setuptools.command.install import install as _install
 from setuptools.command.build_py import build_py as _build
 
 
-VERSION = "1.2"
 VMTKPATH = "../../../vmtk-build/Install"
+
+CLASSIFIERS = ["Development Status :: 5 - Production/Stable",
+               "Environment :: Console",
+               "Intended Audience :: Science/Research",
+               "License :: OSI Approved :: BSD License",
+               "Operating System :: MacOS",
+               "Operating System :: POSIX :: Linux",
+               "Operating System :: Microsoft :: Windows :: Windows 7",
+               "Programming Language :: C++",
+               "Programming Language :: Python :: 2 :: Only",
+               "Topic :: Scientific/Engineering"]
+
+# Description
+description = "vmtk - The Vascular Modeling Toolkit"
+fid = file('README', 'r')
+long_description = fid.read()
+fid.close()
+idx = max(0, long_description.find("vmtk - The Vascular Modeling Toolkit"))
+long_description = long_description[idx:]
+
+NAME                = 'vmtk'
+MAINTAINER          = "Luca Antiga"
+MAINTAINER_EMAIL    = "luca.antiga@orobix.com"
+DESCRIPTION         = description
+LONG_DESCRIPTION    = long_description
+URL                 = "https://github.com/daron1337/jBessel"
+DOWNLOAD_URL        = "http://pypi.python.org/pypi/jBessel"
+LICENSE             = "BSD"
+CLASSIFIERS         = CLASSIFIERS
+AUTHOR              = "Luca Antiga"
+AUTHOR_EMAIL        = "luca.antiga@orobix.com"
+PLATFORMS           = "Linux/MacOsX"
+ISRELEASED          = True
+VERSION             = '1.2'
 
 def list_files(directory):
     '''A specialized version of os.walk() that list files only in the current directory
@@ -75,7 +108,7 @@ class vmtk_install(_install):
         vmtkHomeEnvironmentVariable="VMTKHOME=%s" % package_path
         vmtkPathEnvironmentVariable="export PATH=$VMTKHOME/bin:$PATH"
         vmtkPythonPath="export PYTHONPATH=$VMTKHOME/vmtk/lib:$PYTHONPATH"
-        append_decision = raw_input('Do you want to append vmtk environment variables in your .bash_profile? Y/N: ')
+        append_decision = raw_input('Do you want to append vmtk environment variables in your .bash_profile? YES/n: ')
         while True:
             if append_decision.lower() == 'y':
                 if sys.platform == 'darwin': 
@@ -88,12 +121,12 @@ class vmtk_install(_install):
                         bash_profile.write(vmtkPythonPath+'\n')
                 elif sys.platform == 'linux2':
                     ldEnvironmentVariable="export LD_LIBRARY_PATH=$VMTKHOME/vmtk/lib:$LD_LIBRARY_PATH"
-                    with open(home+'/.bash_profile','aw') as bash_profile:
-                        bash_profile.write('\n#VMTK\n')
-                        bash_profile.write(vmtkHomeEnvironmentVariable+'\n')
-                        bash_profile.write(vmtkPathEnvironmentVariable+'\n')
-                        bash_profile.write(ldEnvironmentVariable+'\n')
-                        bash_profile.write(vmtkPythonPath+'\n')
+                    with open(home+'/.bashrc','aw') as bashrc:
+                        bashrc.write('\n#VMTK\n')
+                        bashrc.write(vmtkHomeEnvironmentVariable+'\n')
+                        bashrc.write(vmtkPathEnvironmentVariable+'\n')
+                        bashrc.write(ldEnvironmentVariable+'\n')
+                        bashrc.write(vmtkPythonPath+'\n')
                 else:
                     #WINDOWS
                     pass
@@ -101,20 +134,29 @@ class vmtk_install(_install):
             elif append_decision.lower() == 'n':
                 break
             else:
-                append_decision = raw_input('Do you want to append vmtk environment variables in your .bash_profile? Y/N: ')
+                append_decision = raw_input('Do you want to append vmtk environment variables in your .bash_profile? YES/n: ')
 
-setup(
-    name = "vmtk",
-    version = VERSION,
-    description='The Vascular Modeling Toolkit',
-    url='http://www.vmtk.org',
-    cmdclass={'install':vmtk_install, 'vmtk_build':vmtk_build},
-    packages = find_packages(),
-    zip_safe=False,
-    data_files=[('bin', [f for f in glob.glob(os.path.join("bin", '*'))])],
-    package_data = {
-       "vmtk": ["lib/*.so*","lib/*.*lib*"],
-       "vtk": ["lib/*.so*"],
-      },
-    scripts = ['vmtk_post_install.py']
+
+setup(name=NAME,
+      maintainer=MAINTAINER,
+      maintainer_email=MAINTAINER_EMAIL,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      url=URL,
+      download_url=DOWNLOAD_URL,
+      license=LICENSE,
+      classifiers=CLASSIFIERS,
+      author=AUTHOR,
+      author_email=AUTHOR_EMAIL,
+      platforms=PLATFORMS,
+      version=VERSION,
+      cmdclass={'install':vmtk_install, 'vmtk_build':vmtk_build},
+      packages = find_packages(),
+      zip_safe=False,
+      data_files=[('bin', [f for f in glob.glob(os.path.join("bin", '*'))])],
+      package_data = {
+         "vmtk": ["lib/*.so*","lib/*.*lib*"],
+         "vtk": ["lib/*.so*"],
+        },
+      scripts = ['vmtk_post_install.py']
     )
--- distribution/egg/vmtk_post_install.py
+++ distribution/egg/vmtk_post_install.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     vmtkHomeEnvironmentVariable="VMTKHOME=%s" % package_path
     vmtkPathEnvironmentVariable="export PATH=$VMTKHOME/bin:$PATH"
     vmtkPythonPath="export PYTHONPATH=$VMTKHOME/vmtk/lib:$PYTHONPATH"
-    append_decision = raw_input('Do you want to append vmtk environment variables in your .bash_profile? Y/N: ')
+    append_decision = raw_input('Do you want to append vmtk environment variables in your .bash_profile? YES/n: ')
     while True:
         if append_decision.lower() == 'y':
             if sys.platform == 'darwin': 
@@ -33,12 +33,12 @@ if __name__ == '__main__':
                     bash_profile.write(vmtkPythonPath+'\n')
             elif sys.platform == 'linux2':
                 ldEnvironmentVariable="export LD_LIBRARY_PATH=$VMTKHOME/vmtk/lib"
-                with open(home+'/.bash_profile','aw') as bash_profile:
-                    bash_profile.write('\n#VMTK\n')
-                    bash_profile.write(vmtkHomeEnvironmentVariable+'\n')
-                    bash_profile.write(vmtkPathEnvironmentVariable+'\n')
-                    bash_profile.write(ldEnvironmentVariable+'\n')
-                    bash_profile.write(vmtkPythonPath+'\n')
+                with open(home+'/.bashrc','aw') as bashrc:
+                    bashrc.write('\n#VMTK\n')
+                    bashrc.write(vmtkHomeEnvironmentVariable+'\n')
+                    bashrc.write(vmtkPathEnvironmentVariable+'\n')
+                    bashrc.write(ldEnvironmentVariable+'\n')
+                    bashrc.write(vmtkPythonPath+'\n')
             else:
                 #WINDOWS
                 pass
@@ -46,4 +46,4 @@ if __name__ == '__main__':
         elif append_decision.lower() == 'n':
             break
         else:
-            append_decision = raw_input('Do you want to append vmtk environment variables in your .bash_profile? Y/N: ' )
+            append_decision = raw_input('Do you want to append vmtk environment variables in your .bash_profile? YES/n: ' )
