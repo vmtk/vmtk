cda33c4b5636ce64b6b384a4709c61dd5a627ea7
daron1337 <simone.manini@gmail.com>
2014-01-20 09:05:02 +0100

    fixed setup path files for windows

--- distribution/egg/setup.py
+++ distribution/egg/setup.py
@@ -7,7 +7,7 @@ from setuptools.command.install import install as _install
 from setuptools.command.build_py import build_py as _build
 
 
-VMTKPATH = "../../../vmtk-build/Install"
+VMTKPATH = "../../../vmtk_build/Install"
 
 CLASSIFIERS = ["Development Status :: 5 - Production/Stable",
                "Environment :: Console",
@@ -58,41 +58,42 @@ class vmtk_build(_build):
         
         #copying install directory 
         try:
-            shutil.copytree(vmtk_path+'/bin','bin', ignore=shutil.ignore_patterns('Python'))
+            shutil.copytree(os.path.join(vmtk_path,'bin'),'bin', ignore=shutil.ignore_patterns('Python'))
         except OSError:
             shutil.rmtree('bin')
-            shutil.copytree(vmtk_path+'/bin','bin', ignore=shutil.ignore_patterns('Python'))
-              
+            shutil.copytree(os.path.join(vmtk_path,'bin'),'bin', ignore=shutil.ignore_patterns('Python'))
+        
         try:
-            shutil.copytree(vmtk_path+'/bin/Python/vtk','vtk')
+            shutil.copytree(os.path.join(vmtk_path,'bin','Python','vtk'),'vtk')
         except OSError:
             shutil.rmtree('vtk')
-            shutil.copytree(vmtk_path+'/bin/Python/vtk','vtk')
+            shutil.copytree(os.path.join(vmtk_path,'bin','Python','vtk'),'vtk')
+        
         try:
-            shutil.copytree(vmtk_path+'/lib/vmtk/vmtk','vmtk')
+            shutil.copytree(os.path.join(vmtk_path,'lib','vmtk','vmtk'),'vmtk')
         except OSError:
             shutil.rmtree('vmtk')
-            shutil.copytree(vmtk_path+'/lib/vmtk/vmtk','vmtk')
+            shutil.copytree(os.path.join(vmtk_path,'lib','vmtk','vmtk'),'vmtk')
         
-        shutil.copytree(vmtk_path+'/lib', 'vmtk/lib', symlinks=True, ignore=shutil.ignore_patterns('cmake'))
+        shutil.copytree(os.path.join(vmtk_path,'lib'), os.path.join('vmtk','lib'), symlinks=True, ignore=shutil.ignore_patterns('cmake'))
         
-        for file_to_move in list_files('vmtk/lib/vmtk'):
-            shutil.copy('vmtk/lib/vmtk/'+file_to_move, 'vmtk/lib/'+file_to_move)
-        shutil.rmtree('vmtk/lib/vmtk')
+        for file_to_move in list_files(os.path.join('vmtk','lib','vmtk')):
+            shutil.copy(os.path.join('vmtk','lib','vmtk',file_to_move),os.path.join('vmtk','lib',file_to_move))
+        shutil.rmtree(os.path.join('vmtk','lib','vmtk'))
         
-        for file_to_move in list_files('vmtk/lib/vtk-5.10'):
-            shutil.copy('vmtk/lib/vtk-5.10/'+file_to_move, 'vmtk/lib/'+file_to_move)
-        shutil.rmtree('vmtk/lib/vtk-5.10')
+        for file_to_move in list_files(os.path.join('vmtk','lib','vtk-5.10')):
+            shutil.copy(os.path.join('vmtk','lib','vtk-5.10',file_to_move),os.path.join('vmtk','lib',file_to_move))
+        shutil.rmtree(os.path.join('vmtk','lib','vtk-5.10'))
         
         for file_to_move in list_files('vmtk'):
-            if file_to_move.endswith('.so'):
-                shutil.move('vmtk/'+file_to_move, 'vmtk/lib/'+file_to_move)
-
-        for file_to_unlink in list_files('vmtk/lib'):
+            if file_to_move.endswith('.so') or file_to_move.endswith('.pyd'):
+                shutil.move(os.path.join('vmtk',file_to_move),os.path.join('vmtk','lib',file_to_move))
+        
+        for file_to_unlink in list_files(os.path.join('vmtk','lib')):
             if file_to_unlink == 'hints':
-                os.remove('vmtk/lib/'+file_to_unlink)
-            if os.path.islink('vmtk/lib/'+file_to_unlink):
-                os.unlink('vmtk/lib/'+file_to_unlink)
+                os.remove(os.path.join('vmtk','lib',file_to_unlink))
+            if os.path.islink(os.path.join('vmtk','lib',file_to_unlink)):
+                os.unlink(os.path.join('vmtk','lib',file_to_unlink))
                         
 class vmtk_install(_install):
     def run(self):
@@ -101,53 +102,54 @@ class vmtk_install(_install):
         package_name = 'vmtk-'+VERSION+'-py2.7.egg-info'
         package_path = self.install_lib+package_name
         
-        print "Please run this script with sudo\n"
-        print "chmod+x on binary scripts..."
-        bin_path = package_path+'/bin'      
-        os.system('chmod -R +x '+bin_path)
-        
-        vmtkHomeEnvironmentVariable="VMTKHOME=%s" % package_path
-        vmtkPathEnvironmentVariable="export PATH=$VMTKHOME/bin:$PATH"
-        vmtkPythonPath="export PYTHONPATH=$VMTKHOME/vmtk/lib:$PYTHONPATH"
-        if sys.platform == 'darwin':
-            append_decision = raw_input('Do you want to append vmtk environment variables in your .bash_profile? yes/no: ')
-        elif sys.platform == 'linux2':
-            append_decision = raw_input('Do you want to append vmtk environment variables in your .bashrc? yes/no: ')
-        else:
-            #WINDOWS
-            pass
-        while True:
-            if append_decision.lower() == 'y' or append_decision.lower() == 'yes':
-                if sys.platform == 'darwin': 
-                    ldEnvironmentVariable="export DYLD_LIBRARY_PATH=$VMTKHOME/vmtk/lib:$DYLD_LIBRARY_PATH"
-                    with open(home+'/.bash_profile','aw') as bash_profile:
-                        bash_profile.write('\n#VMTK\n')
-                        bash_profile.write(vmtkHomeEnvironmentVariable+'\n')
-                        bash_profile.write(vmtkPathEnvironmentVariable+'\n')
-                        bash_profile.write(ldEnvironmentVariable+'\n')
-                        bash_profile.write(vmtkPythonPath+'\n')
-                elif sys.platform == 'linux2':
-                    ldEnvironmentVariable="export LD_LIBRARY_PATH=$VMTKHOME/vmtk/lib:$LD_LIBRARY_PATH"
-                    with open(home+'/.bashrc','aw') as bashrc:
-                        bashrc.write('\n#VMTK\n')
-                        bashrc.write(vmtkHomeEnvironmentVariable+'\n')
-                        bashrc.write(vmtkPathEnvironmentVariable+'\n')
-                        bashrc.write(ldEnvironmentVariable+'\n')
-                        bashrc.write(vmtkPythonPath+'\n')
-                else:
-                    #WINDOWS
-                    pass
-                break
-            elif append_decision.lower() == 'n' or append_decision.lower() == 'no':
-                break
+        if sys.platform != "win32":
+            print "Please run this script with sudo\n"
+            print "chmod+x on binary scripts..."
+            bin_path = package_path+'/bin'      
+            os.system('chmod -R +x '+bin_path)
+            
+            vmtkHomeEnvironmentVariable="VMTKHOME=%s" % package_path
+            vmtkPathEnvironmentVariable="export PATH=$VMTKHOME/bin:$PATH"
+            vmtkPythonPath="export PYTHONPATH=$VMTKHOME/vmtk/lib:$PYTHONPATH"
+            if sys.platform == 'darwin':
+                append_decision = raw_input('Do you want to append vmtk environment variables in your .bash_profile? yes/no: ')
+            elif sys.platform == 'linux2':
+                append_decision = raw_input('Do you want to append vmtk environment variables in your .bashrc? yes/no: ')
             else:
-                if sys.platform == 'darwin':
-                    append_decision = raw_input('Do you want to append vmtk environment variables in your .bash_profile? yes/no: ')
-                elif sys.platform == 'linux2':
-                    append_decision = raw_input('Do you want to append vmtk environment variables in your .bashrc? yes/no: ')
+                #WINDOWS
+                pass
+            while True:
+                if append_decision.lower() == 'y' or append_decision.lower() == 'yes':
+                    if sys.platform == 'darwin': 
+                        ldEnvironmentVariable="export DYLD_LIBRARY_PATH=$VMTKHOME/vmtk/lib:$DYLD_LIBRARY_PATH"
+                        with open(home+'/.bash_profile','aw') as bash_profile:
+                            bash_profile.write('\n#VMTK\n')
+                            bash_profile.write(vmtkHomeEnvironmentVariable+'\n')
+                            bash_profile.write(vmtkPathEnvironmentVariable+'\n')
+                            bash_profile.write(ldEnvironmentVariable+'\n')
+                            bash_profile.write(vmtkPythonPath+'\n')
+                    elif sys.platform == 'linux2':
+                        ldEnvironmentVariable="export LD_LIBRARY_PATH=$VMTKHOME/vmtk/lib:$LD_LIBRARY_PATH"
+                        with open(home+'/.bashrc','aw') as bashrc:
+                            bashrc.write('\n#VMTK\n')
+                            bashrc.write(vmtkHomeEnvironmentVariable+'\n')
+                            bashrc.write(vmtkPathEnvironmentVariable+'\n')
+                            bashrc.write(ldEnvironmentVariable+'\n')
+                            bashrc.write(vmtkPythonPath+'\n')
+                    else:
+                        #WINDOWS
+                        pass
+                    break
+                elif append_decision.lower() == 'n' or append_decision.lower() == 'no':
+                    break
                 else:
-                    #WINDOWS
-                    pass
+                    if sys.platform == 'darwin':
+                        append_decision = raw_input('Do you want to append vmtk environment variables in your .bash_profile? yes/no: ')
+                    elif sys.platform == 'linux2':
+                        append_decision = raw_input('Do you want to append vmtk environment variables in your .bashrc? yes/no: ')
+                    else:
+                        #WINDOWS
+                        pass
 
 
 setup(name=NAME,
