5ee3cc06d0695533c2a655cc765131a0ab4168f2
Rick Izzo <rizzo242@gmail.com>
2017-05-12 06:36:34 -0400

    minor changes. reverted to old CMAKELists. still not working

--- distribution/anaconda/build.sh
+++ distribution/anaconda/build.sh
@@ -6,14 +6,7 @@ cd vmtk-build
 
 # build.
 cmake ../vmtk \
-    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-    -DCMAKE_INSTALL_RPATH:STRING="${PREFIX}/lib" \
-    -DPYTHON_EXECUTABLE=${PYTHON} \
-    -DPYTHON_INCLUDE_PATH=${PREFIX}/include/python${PY_VER} \
-    -DPYTHON_LIBRARY=${PREFIX}/lib/${PY_LIB} \
-    -DVTK_INSTALL_PYTHON_MODULE_DIR=${SP_DIR} \
-    -VMTK_INSTALL_BIN_DIR="${PREFIX}/bin" \
-    -VMTK_INSTALL_LIB_DIR="${PREFIX}/lib" \
-    -NOT VMTK_MODULE_INSTALL_LIB_DIR="${PREFIX}/lib/
+    -DCMAKE_INSTALL_PREFIX=$PREFIX
     .. | tee cmake.log 2>&1
+
+make -j 7
