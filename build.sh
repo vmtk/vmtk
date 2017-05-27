fc1369578afdba4efce6d1ae3e4bf2bfed5c4e09
Rick Izzo <rizzo242@gmail.com>
2017-05-27 02:24:17 -0400

    updated location of build files

--- build.sh
+++ build.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 # unpack.
-mkdir vmtk-build
-cd vmtk-build
+mkdir build
+cd build
 
 # build.
-cmake ../vmtk \
+cmake ../ \
     -DLIBRARY_OUTPUT_PATH=$PATH \
     -DEXECUTABLE_OUTPUT_PATH=$PATH \
     -DVMTK_INSTALL_BIN_DIR=$PREFIX/bin \
-    -DVMTK_INSTALL_LIB_DIR=$PREFIX/lib
+    -DVMTK_INSTALL_LIB_DIR=$PREFIX/lib \
+    -DVMTK_ENABLE_DISTRIBUTION=ON
 
 make -j 7
