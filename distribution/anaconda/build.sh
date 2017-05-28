59d8ff7f88792325c5d329709c51d57d85e7b74f
Rick Izzo <rizzo242@gmail.com>
2017-05-27 20:17:19 -0400

    trying to enable distribution targets.

--- distribution/anaconda/build.sh
+++ distribution/anaconda/build.sh
@@ -4,7 +4,8 @@ mkdir vmtk-build
 cd ./vmtk-build
 
 cmake ../ \
-    -DCMAKE_BUILD_TYPE:STRING=Debug
+    -DCMAKE_BUILD_TYPE:STRING=Debug \
+    -DVMTK_ENABLE_DISTRIBUTION:BOOL=ON 
 
 make -j7
 make install
