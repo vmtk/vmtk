58f05d87817d08b4735845c686ad7f4a5378626e
Rick Izzo <rizzo242@gmail.com>
2017-05-27 21:02:01 -0400

    playing around with the install directory. not sure if this is going anywhere

--- distribution/anaconda/build.sh
+++ distribution/anaconda/build.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-mkdir vmtk-build
-cd ./vmtk-build
+mkdir build
+cd build
 
-cmake ../ \
+PYV=`python -c "import sys;t='{v[0]}.{v[1]}'.format(v=list(sys.version_info[:2]));sys.stdout.write(t)";`
+SUPERBUILD_INSTALL_PREFIX="${PREFIX}"
+cmake \
     -DCMAKE_BUILD_TYPE:STRING=Debug \
-    -DVMTK_ENABLE_DISTRIBUTION:BOOL=ON
+    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+    -DPYTHON_EXECUTABLE="${PYTHON}" \
+    -DPYTHON_INCLUDE_DIR="$PREFIX/include/python${PYV}" \
+    -DPYTHON_LIBRARY="$PREFIX/lib/libpython${PYV}.so" ..
 
 make -j7
-make install
