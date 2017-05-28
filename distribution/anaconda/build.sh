970dfa697e50344e51f9f67924b0dbd19c05b302
Rick Izzo <rizzo242@gmail.com>
2017-05-27 22:34:13 -0400

    last try for the night. can't figure out why vmtk won't compile

--- distribution/anaconda/build.sh
+++ distribution/anaconda/build.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
-mkdir build
-cd build
+mkdir ../build
+cd ../build
 
-PYV=`python -c "import sys;t='{v[0]}.{v[1]}'.format(v=list(sys.version_info[:2]));sys.stdout.write(t)";`
-SUPERBUILD_INSTALL_PREFIX="${PREFIX}"
-cmake \
-    -DCMAKE_BUILD_TYPE:STRING=Debug \
-    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-    -DPYTHON_EXECUTABLE="${PYTHON}" \
-    -DPYTHON_INCLUDE_DIR="$PREFIX/include/python${PYV}" \
-    -DPYTHON_LIBRARY="$PREFIX/lib/libpython${PYV}.so" ..
+if [ `uname` == Linux ]; then
+    CC=$PREFIX/bin/gcc CCX=$PREFIX/bin/g++ \
+    cmake ../vmtk-1.4-beta.3 \
+        -DCMAKE_BUILD_TYPE:STRING=debug
+fi
 
 make -j7
