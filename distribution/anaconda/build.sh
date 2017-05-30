82234e470e1782da0d6395d77297110b39f1d825
Rick Izzo <rizzo242@gmail.com>
2017-05-29 23:07:52 -0400

    This actually seems to work.

--- distribution/anaconda/build.sh
+++ distribution/anaconda/build.sh
@@ -1,11 +1,31 @@
 #!/bin/sh
 
+## Program:   VMTK
+## Module:    Anaconda Distribution
+## Language:  Python
+## Date:      May 29, 2017
+
+##   Copyright (c) Luca Antiga, David Steinman. All rights reserved.
+##   See LICENCE file for details.
+
+##      This software is distributed WITHOUT ANY WARRANTY; without even
+##      the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+##      PURPOSE.  See the above copyright notices for more information.
+
+## Note: this class was contributed by
+##       Richard Izzo (Github @rlizzo)
+##       University at Buffalo; The Jacobs Institute
+
+## This file contains the packaging and distribution shell script data for packaging
+## VMTK via the Continuum Analytics Anaconda Python distribution.
+## See https://www.continuum.io/ for distribution info
+
 mkdir vmtk-build
 cd ./vmtk-build
 
 if [ `uname` == Linux ]; then
     cmake ../ \
-        -DCMAKE_BUILD_TYPE:STRING="Release"
+        -DCMAKE_BUILD_TYPE:STRING="Debug"
 
     make -j7
 
