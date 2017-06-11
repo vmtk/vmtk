0db9eb4f21b1e83a65a5b34ec7d0154ac1787d5d
Rick Izzo <rizzo242@gmail.com>
2017-06-10 20:46:36 -0400

    vmtknumpytocenterlines now accepts both scalar and vector PointData array components

--- vmtkScripts/contrib/vmtknumpytocenterlines.py
+++ vmtkScripts/contrib/vmtknumpytocenterlines.py
@@ -63,14 +63,24 @@ class vmtkNumpyToCenterlines(pypes.pypeScript):
             if pointDataDType.find('int') != -1:
                 pointDataArray = vtk.vtkIntArray()
 
-            pointDataArray.SetNumberOfComponents(1)
+            try:
+                pointDataComponents = self.InputDict['PointData'][key].shape[1]
+            except IndexError:
+                pointDataComponents = 1
+
+            pointDataArray.SetNumberOfComponents(pointDataComponents)
             pointDataArray.SetName(key)
 
-            for pointData in self.InputDict['PointData'][key]:
-                pointDataArray.InsertNextValue(pointData)
-
-            polyData.GetPointData().SetActiveScalars(key)
-            polyData.GetPointData().SetScalars(pointDataArray)
+            if pointDataComponents == 1:
+                for pointData in self.InputDict['PointData'][key]:
+                    pointDataArray.InsertNextValue(pointData)
+                polyData.GetPointData().SetActiveScalars(key)
+                polyData.GetPointData().SetScalars(pointDataArray)
+            else:
+                for pointData in self.InputDict['PointData'][key]:
+                    pointDataArray.InsertNextTuple(pointData)
+                polyData.GetPointData().SetActiveVectors(key)
+                polyData.GetPointData().SetVectors(pointDataArray)
 
         cellDataArray = vtk.vtkCellArray()
         numberOfCells = len(self.InputDict['CellData']['CellPointIds'].keys())
