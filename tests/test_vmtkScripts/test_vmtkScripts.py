49cb7acb4bb9ae0512ca25206953c296d3e5619a
Rick Izzo <rizzo242@gmail.com>
2018-01-10 00:40:58 -0500

    added header license file, added placeholder CMakeLists.txt files

--- tests/test_vmtkScripts/test_vmtkScripts.py
+++ tests/test_vmtkScripts/test_vmtkScripts.py
@@ -1,19 +1,16 @@
-#!/usr/bin/env python
-
-## Program:   VMTK Tests
-## Module:    $RCSfile: test_pypes.py,v $
+## Program: VMTK
 ## Language:  Python
-## Date:      $Date: 12 DEC 2018$
-## Version:   $Revision: 0.1 $
+## Date:      January 10, 2018
+## Version:   1.4
 
-##   Copyright (c) Richard Izzo, Luca Antiga. All rights reserved.
+##   Copyright (c) Richard Izzo, Luca Antiga, All rights reserved.
 ##   See LICENCE file for details.
 
 ##      This software is distributed WITHOUT ANY WARRANTY; without even
 ##      the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 ##      PURPOSE.  See the above copyright notices for more information.
 
-## Note: this class was contributed by
+## Note: this code was contributed by
 ##       Richard Izzo (Github @rlizzo)
 ##       University at Buffalo
 
