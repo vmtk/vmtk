a3d1106a45326c52c1eb3b1bdaddd93486b6cbd8
Rick Izzo <rizzo242@gmail.com>
2018-01-10 00:16:29 -0500

    added new test, streamlined others

--- tests/test_vmtkScripts/test_vmtkimagebinarize.py
+++ tests/test_vmtkScripts/test_vmtkimagebinarize.py
@@ -1,69 +1,44 @@
 import pytest
-import os
-from hashlib import sha1
-import vmtk.vmtkimagetonumpy as wrap
 import vmtk.vmtkimagebinarize as binarize
 
-def test_binarize_default_parameters(aorta_image):
+def test_binarize_default_parameters(aorta_image, image_to_sha):
     binarizer = binarize.vmtkImageBinarize()
     binarizer.Image = aorta_image
     binarizer.Execute()
 
-    conv = wrap.vmtkImageToNumpy()
-    conv.Image = binarizer.Image
-    conv.Execute()
-
-    check = conv.ArrayDict['PointData']['ImageScalars'].copy(order='C')
-
-    assert sha1(check).hexdigest() == '3494222d77bf1a4b9be4f709436c6825a29ac181'
+    assert image_to_sha(binarizer.Image) == '3494222d77bf1a4b9be4f709436c6825a29ac181'
 
 
-def test_binarize_simple_threshold(aorta_image):
+def test_binarize_simple_threshold(aorta_image, image_to_sha):
     binarizer = binarize.vmtkImageBinarize()
     binarizer.Image = aorta_image
     binarizer.Threshold = 500
     binarizer.Execute()
 
-    conv = wrap.vmtkImageToNumpy()
-    conv.Image = binarizer.Image
-    conv.Execute()
-
-    check = conv.ArrayDict['PointData']['ImageScalars'].copy(order='C')
-
-    assert sha1(check).hexdigest() == 'b2950d439179f1637b3297df81e3adfcfd878d07'
+    assert image_to_sha(binarizer.Image) == 'b2950d439179f1637b3297df81e3adfcfd878d07'
 
 
-def test_binarize_modify_lower_label(aorta_image):
+def test_binarize_modify_lower_label(aorta_image, image_to_sha):
     binarizer = binarize.vmtkImageBinarize()
     binarizer.Image = aorta_image
     binarizer.Threshold = 500
     binarizer.LowerLabel = -1
     binarizer.Execute()
 
-    conv = wrap.vmtkImageToNumpy()
-    conv.Image = binarizer.Image
-    conv.Execute()
+    assert image_to_sha(binarizer.Image) == '553eaffc0a6d617b52bb4a3a9b9bbcc61236424b'
 
-    check = conv.ArrayDict['PointData']['ImageScalars'].copy(order='C')
 
-    assert sha1(check).hexdigest() == '553eaffc0a6d617b52bb4a3a9b9bbcc61236424b'
-
-def test_binarize_modify_upper_label(aorta_image):
+def test_binarize_modify_upper_label(aorta_image, image_to_sha):
     binarizer = binarize.vmtkImageBinarize()
     binarizer.Image = aorta_image
     binarizer.Threshold = 500
     binarizer.UpperLabel = 7
     binarizer.Execute()
 
-    conv = wrap.vmtkImageToNumpy()
-    conv.Image = binarizer.Image
-    conv.Execute()
+    assert image_to_sha(binarizer.Image) == 'd76f020b72fab68adcde0f1b5d269c2b731c3c21'
 
-    check = conv.ArrayDict['PointData']['ImageScalars'].copy(order='C')
 
-    assert sha1(check).hexdigest() == 'd76f020b72fab68adcde0f1b5d269c2b731c3c21'
-
-def test_binarize_modify_upper_and_lower_label(aorta_image):
+def test_binarize_modify_upper_and_lower_label(aorta_image, image_to_sha):
     binarizer = binarize.vmtkImageBinarize()
     binarizer.Image = aorta_image
     binarizer.Threshold = 500
@@ -71,10 +46,4 @@ def test_binarize_modify_upper_and_lower_label(aorta_image):
     binarizer.LowerLabel = 3
     binarizer.Execute()
 
-    conv = wrap.vmtkImageToNumpy()
-    conv.Image = binarizer.Image
-    conv.Execute()
-
-    check = conv.ArrayDict['PointData']['ImageScalars'].copy(order='C')
-
-    assert sha1(check).hexdigest() == 'c250c060b43e33f566ccd24d667b6714793cdf7a'
+    assert image_to_sha(binarizer.Image) == 'c250c060b43e33f566ccd24d667b6714793cdf7a'
--- tests/test_vmtkScripts/test_vmtkimagereader.py
+++ tests/test_vmtkScripts/test_vmtkimagereader.py
@@ -1,48 +1,36 @@
 import pytest
 import os
-from hashlib import sha1
-import vmtk.vmtkimagetonumpy as wrap
+import vmtk.vmtkimagereader as imagereader
 
-def test_read_mha_image(test_data):
-    import vmtk.vmtkimagereader as r
-    reader = r.vmtkImageReader()
+def test_read_mha_image(test_data, image_to_sha):
+    reader = imagereader.vmtkImageReader()
     reader.InputFileName = os.path.join(test_data, 'aorta.mha')
     reader.Execute()
 
-    conv = wrap.vmtkImageToNumpy()
-    conv.Image = reader.Image
-    conv.Execute()
-
-    check = conv.ArrayDict['PointData']['ImageScalars'].copy(order='C')
-
-    assert sha1(check).hexdigest() == '08c6d50899ecfbe5ad42d9c17703b54f2b4fe428'
+    assert image_to_sha(reader.Image) == '08c6d50899ecfbe5ad42d9c17703b54f2b4fe428'
 
 
-def test_read_dicom_image(test_data):
-    import vmtk.vmtkimagereader as r
-    reader = r.vmtkImageReader()
+def test_read_dicom_image(test_data, image_to_sha):
+    reader = imagereader.vmtkImageReader()
     reader.InputFileName = os.path.join(test_data, 'mr-dicom.dcm')
     reader.Execute()
 
-    conv = wrap.vmtkImageToNumpy()
-    conv.Image = reader.Image
-    conv.Execute()
-
-    check = conv.ArrayDict['PointData']['ImageScalars'].copy(order='C')
-
-    assert sha1(check).hexdigest() == '2ceb5680a790e14b8f67efc02d4e63e05bb77bad'
+    assert image_to_sha(reader.Image) == '2ceb5680a790e14b8f67efc02d4e63e05bb77bad'
 
 
 def test_read_vti_image(test_data):
-    import vmtk.vmtkimagereader as r
-    reader = r.vmtkImageReader()
+    import vmtk.vmtkimagetonumpy as imagetonumpy
+    from hashlib import sha1
+
+    reader = imagereader.vmtkImageReader()
     reader.InputFileName = os.path.join(test_data, 'vase.vti')
     reader.Execute()
 
-    conv = wrap.vmtkImageToNumpy()
-    conv.Image = reader.Image
-    conv.Execute()
-
-    check = conv.ArrayDict['PointData']['SLCImage'].copy(order='C')
+    # need to do a manual conversion since the array point data key is non
+    # standard for this test data object
+    converter = imagetonumpy.vmtkImageToNumpy()
+    converter.Image = reader.Image
+    converter.Execute()
+    check = converter.ArrayDict['PointData']['SLCImage'].copy(order='C')
 
     assert sha1(check).hexdigest() == '1a7bd4ca031bccd322bea06ff1fa6d69e45c63db'
