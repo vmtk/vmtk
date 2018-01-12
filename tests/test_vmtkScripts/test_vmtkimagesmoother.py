854900fe09e739eca7c244c98994e1af5973f2f0
Rick Izzo <rizzo242@gmail.com>
2018-01-12 16:10:23 -0500

    finished converting to compare image method when appropriate

--- tests/test_vmtkScripts/test_vmtkimagesmoother.py
+++ tests/test_vmtkScripts/test_vmtkimagesmoother.py
@@ -18,27 +18,30 @@ import pytest
 import vmtk.vmtkimagesmoothing as imagesmoothing
 
 
-def test_default_smoothing(aorta_image, image_to_sha):
+def test_default_smoothing(aorta_image, compare_images):
+    name = __name__ + '_test_default_smoothing.mha'
     smoother = imagesmoothing.vmtkImageSmoothing()
     smoother.Image = aorta_image
     smoother.Execute()
 
-    assert image_to_sha(smoother.Image) == '9885dd0ab5ecbc32f1c0b738a130af63fd559652'
+    assert compare_images(smoother.Image, name) == True
 
 
-def test_smoothing_high_standard_deviation(aorta_image, image_to_sha):
+def test_smoothing_high_standard_deviation(aorta_image, compare_images):
+    name = __name__ + '_test_smoothing_high_standard_deviation.mha'
     smoother = imagesmoothing.vmtkImageSmoothing()
     smoother.Image = aorta_image
     smoother.StandardDeviation = 7.6
     smoother.Execute()
 
-    assert image_to_sha(smoother.Image) == 'b5a71c71bd60e532a9228d21117367d7547669bb'
+    assert compare_images(smoother.Image, name) == True
 
 
-def test_lower_radius_factor(aorta_image, image_to_sha):
+def test_lower_radius_factor(aorta_image, compare_images):
+    name = __name__ + '_test_lower_radius_factor.mha'
     smoother = imagesmoothing.vmtkImageSmoothing()
     smoother.Image = aorta_image
     smoother.RadiusFactor = 3.4
     smoother.Execute()
 
-    assert image_to_sha(smoother.Image) == '9fbb09c29ab9ce44e74d412a0d6c668fc9c45be9'
+    assert compare_images(smoother.Image, name) == True
--- tests/test_vmtkScripts/test_vmtkimagevoiselector.py
+++ tests/test_vmtkScripts/test_vmtkimagevoiselector.py
@@ -17,6 +17,18 @@
 import pytest
 import vmtk.vmtkimagevoiselector as voiselector
 
+#TODO: Why does this fail, but the sha method passes?
+@pytest.mark.skip(reason='This assertion fails for an unknown reason')
+def test_voi_selector(aorta_image, compare_images):
+    name = __name__ + '_test_voi_selector.mha'
+    selector = voiselector.vmtkImageVOISelector()
+    selector.Image = aorta_image
+    selector.Interactive = 0
+    selector.BoxBounds = [157.0, 186.0, 31.0, 67.0, 10.0, 20.0]
+    selector.Execute()
+
+    assert compare_images(selector.Image, name) == True
+
 def test_voi_selector(aorta_image, image_to_sha):
     selector = voiselector.vmtkImageVOISelector()
     selector.Image = aorta_image
@@ -25,4 +37,3 @@ def test_voi_selector(aorta_image, image_to_sha):
     selector.Execute()
 
     assert image_to_sha(selector.Image) == 'f834f6ba5a9e8caf86fb9e0f951650e5adc627cc'
-
