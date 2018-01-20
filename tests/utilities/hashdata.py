840d1e0a05e2a26716bc59c9223948286749eee2
Rick Izzo <rizzo242@gmail.com>
2018-01-19 22:44:01 -0500

    added method to create md5 hash of file

---
+++ tests/utilities/hashdata.py
@@ -0,0 +1,91 @@
+## Program: VMTK
+## Language:  Python
+## Date:      January 10, 2018
+## Version:   1.4
+
+##   Copyright (c) Richard Izzo, Luca Antiga, All rights reserved.
+##   See LICENCE file for details.
+
+##      This software is distributed WITHOUT ANY WARRANTY; without even
+##      the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+##      PURPOSE.  See the above copyright notices for more information.
+
+## Note: this code was contributed by
+##       Richard Izzo (Github @rlizzo)
+##       University at Buffalo
+
+# This script is designed to take data from an input directory, read all
+# files, and write a file  with the name "ifile.ext" md5 hash to a seperate
+# file (keeping the relative directory structure the same) in a seperate
+# directory with the file name "ifile.ext.md5"
+
+import argparse
+import os
+import sys 
+import glob
+import hashlib
+
+parser = argparse.ArgumentParser()
+parser.add_argument("idir", type=str, 
+    help="the input directory for all data files to hash")
+parser.add_argument("odir", type=str, 
+    help="the output directory to store hashed file digests in the same \
+     relative directory")
+args = parser.parse_args()
+
+
+# handle relative path names.
+if args.idir.startswith('./'):
+    args.idir = args.idir.replace('./', '')
+    args.idir = os.path.join(os.getcwd(), args.idir)
+elif args.idir.startswith('../'):
+    args.idir = args.idir.replace('../', '')
+    # join dir name one directory up from calling directory
+    args.idir = os.path.join(os.path.dirname(os.getcwd()), args.idir)
+
+#handle relative path names
+if args.odir.startswith('./'):
+    args.odir = args.odir.replace('./', '')
+    args.odir = os.path.join(os.getcwd(), args.odir)
+elif args.odir.startswith('../'):
+    args.odir = args.odir.replace('../', '')
+    # join dir name one directory up from calling directory
+    args.odir = os.path.join(os.path.dirname(os.getcwd()), args.odir)
+
+'''
+this next block finds the full path to files, and a relative path for the same file to place in the output directory.
+file in hidden (dot) directories are ignored, as are mark-down files
+
+Examples:
+inFilepath: /Users/rick/projects/vmtk/vmtk-test-data/input/aorta-surface.stl
+relpath: input/aorta-surface.stl.md5
+outFilepath: /Users/rick/projects/vmtk/vmtk/tests/data/input/aorta-surface.stl.md5
+
+inFilepath: /Users/rick/projects/vmtk/vmtk-test-data/surfacereference/test_vmtksurfacesmoothing_test_laplace_change_relaxation.vtp
+relpath: surfacereference/test_vmtksurfacesmoothing_test_laplace_change_relaxation.vtp.md5
+outFilepath: /Users/rick/projects/vmtk/vmtk/tests/data/surfacereference/test_vmtksurfacesmoothing_test_laplace_change_relaxation.vtp.md5
+''' 
+for root, dirs, files in os.walk(args.idir):
+    # exclude directories and files in hidden folders ('dot' directories)
+    files = [f for f in files if not f[0] == '.']
+    dirs[:] = [d for d in dirs if not d[0] == '.']
+    for ifile in files:
+        # full path to input file
+        inFilepath = os.path.join(root, ifile) 
+        if inFilepath.lower().endswith('.md'):
+            continue
+        else:
+            # relative path of input file to place in output directory
+            relpath = inFilepath.replace(args.idir, '') + '.md5'
+            outFilepath = os.path.join(args.odir, relpath)
+
+        with open(inFilepath, 'rb') as f:
+            data = f.read()
+            inFilehash = hashlib.md5(data).hexdigest()
+
+        if not os.path.exists(os.path.dirname(outFilepath)):
+            os.makedirs(os.path.dirname(outFilepath))
+
+        with open(outFilepath, "w+") as text_file:
+            print(inFilehash, file=text_file)
+        print(f'processed file {relpath}')
