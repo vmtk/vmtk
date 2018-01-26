d6cdcdecbf8a73e462e893cb3e9ed58b7e8f3f91
Rick Izzo <rizzo242@gmail.com>
2018-01-26 02:22:15 -0500

    bugfixes builds on travis and appveyor work.
    linux, mac, and windows packages are all building and passing tests
    successfully.

---
+++ README.md
@@ -0,0 +1,14 @@
+## [VMTK - the Vascular Modeling Toolkit](http://vmtk.org)
+
+[![Build Status](https://travis-ci.org/vmtk/vmtk.svg?branch=master)](https://travis-ci.org/vmtk/vmtk)
+[![Build status](https://ci.appveyor.com/api/projects/status/3u6nupu2r47qbesq/branch/master?svg=true)](https://ci.appveyor.com/project/rlizzo/vmtk-bh6uc/branch/master)
+
+Introduction
+============
+
+The Vascular Modeling Toolkit is a collection of libraries and 
+tools for 3D reconstruction, geometric analysis, mesh generation 
+and surface data analysis for image-based modeling of blood vessels.
+
+See vmtk.org for details.
+
--- condabuild/travis/install.sh
+++ condabuild/travis/install.sh
@@ -1,8 +1,8 @@
 # Install and set up miniconda.
-if [ $TRAVIS_OS_NAME == "linux" ]; then sudo apt-get update && sudo apt-get install -yqq git wget libxt-dev libgl1-mesa-glx libgl1-mesa-dev libglapi-mesa libosmesa-dev build-essential; fi
+# if [ $TRAVIS_OS_NAME == "linux" ]; then sudo apt-get update && sudo apt-get install -yqq git wget libxt-dev libgl1-mesa-glx libgl1-mesa-dev libglapi-mesa libosmesa-dev build-essential; fi
 if [ $TRAVIS_OS_NAME == "linux" ]; then wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh; fi
 if [ $TRAVIS_OS_NAME == "osx" ]; then wget http://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh; fi
-if [ $TRAVIS_OS_NAME == "osx" ]; then git clone https://github.com/phracker/MacOSX-SDKs.git; fi
+if [ $TRAVIS_OS_NAME == "osx" ]; then cd $HOME && git clone https://github.com/phracker/MacOSX-SDKs.git && cd $TRAVIS_BUILD_DIR; fi
 bash miniconda.sh -b -p $CONDA_INSTALL_LOCN
 export PATH=${CONDA_INSTALL_LOCN}/bin:$PATH
 conda config --set always_yes true
@@ -15,15 +15,3 @@ conda config --prepend channels vmtk/label/dev
 # To ease debugging, list installed packages
 conda info -a
 conda list
-# - |
-#   Only upload if this is NOT a pull request.
-#   if [ "$TRAVIS_PULL_REQUEST" = "false" ] && \
-#      [ $TRAVIS_REPO_SLUG = "bccp/conda-channel-bccp" ] && \
-#      [ "$TRAVIS_BRANCH" == "master" ]; then
-#       UPLOAD="--user $DESTINATION_CONDA_CHANNEL --token $BINSTAR_TOKEN";
-#       conda config --set anaconda_upload true
-#       echo "Uploading enabled";
-#   else
-#       echo "Uplading disabled";
-#       UPLOAD="";
-#   fi
