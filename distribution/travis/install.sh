cc582908f3017fc5ea118f8f40ada6ecb8b38cdc
Rick Izzo <rizzo242@gmail.com>
2018-02-19 18:18:58 -0500

    speed up macos download for 10.9sdk

--- distribution/travis/install.sh
+++ distribution/travis/install.sh
@@ -17,7 +17,7 @@
 # Install and set up miniconda.
 if [ $TRAVIS_OS_NAME == "linux" ]; then wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh; fi
 if [ $TRAVIS_OS_NAME == "osx" ]; then wget http://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh; fi
-if [ $TRAVIS_OS_NAME == "osx" ]; then cd $HOME && git clone https://github.com/phracker/MacOSX-SDKs.git && cd $TRAVIS_BUILD_DIR; fi
+if [ $TRAVIS_OS_NAME == "osx" ]; then cd $HOME && mkdir MacOSX-SDKs && cd MacOSX-SDKs && wget https://github.com/phracker/MacOSX-SDKs/releases/download/10.13/MacOSX10.9.sdk.tar.xz && tar -xf MacOSX10.9.sdk.tar.xz && rm MacOSX10.9.sdk.tar.xz && cd $TRAVIS_BUILD_DIR; fi
 bash miniconda.sh -b -p $CONDA_INSTALL_LOCN
 export PATH=${CONDA_INSTALL_LOCN}/bin:$PATH
 conda config --set always_yes true
