8740a8272115d1786dad247a06a2560ae86123ce
Rick Izzo <rizzo242@gmail.com>
2018-04-10 04:33:28 -0400

    vastly improved method of rendering and speed.
    Off the start vtkPolyDataConnectivityFilter is ran only once to extract
    and label all regions. From there it is called again to extract only the
    regionId which was selected by the user. This is added to it's own
    actor in the scene (rather than adding every component as
    individual actors). Unlimited selections and 'undos' can be performed.

--- vmtkScripts/vmtksurfaceconnectivityselector.py
+++ vmtkScripts/vmtksurfaceconnectivityselector.py
@@ -31,48 +31,82 @@ class vmtkPickPointSeedSelector(object):
         self.InputInfo = None
         self._OutputPolyDataList = []
 
-        self.PickedSeedIds = vtk.vtkIdList()
-        self.PickedSeeds = vtk.vtkPolyData()
         self.vmtkRenderer = None
         self.OwnRenderer = 0
         self.Script = None
+        self.CurrentPickedActor = None
+        self.CurrentPickedPolyData = None
+        self.PastPickedActorList = []
         
 
     def UndoCallback(self, obj):
-        self.InitializeSeeds()
-        self.PickedSeeds.Modified()
+        # The execution order is extremly important in this method. Do not change if possible. 
         self.vmtkRenderer.RenderWindow.Render()
+        # First remove the vtkPolyDataObject from the list where it is being stored.
+        # by default we remove the last element selected first
         self._OutputPolyDataList.pop()
-        self.PickedActor.GetProperty().SetColor(0.7, 0.7, 0.7)
-        self.PickedActor.GetProperty().SetDiffuse(0.9)
-        self.PickedActor.GetProperty().SetSpecular(0.2)
+        # the actor instance is stored within the PastPickedActorList object. We tell the renderer
+        # to remove the actor from the scene, then remove that actor from the list. 
+        self.vmtkRenderer.Renderer.RemoveActor(self.PastPickedActorList[-1])
+        self.PastPickedActorList.pop()
+        # update the scene in the render window
+        self.vmtkRenderer.RenderWindow.Render()
         return
 
 
     def PickCallback(self, obj):
-        picker = vtk.vtkPropPicker()
+        picker = vtk.vtkPointPicker()
         eventPosition = self.vmtkRenderer.RenderWindowInteractor.GetEventPosition()
         result = picker.Pick(float(eventPosition[0]),float(eventPosition[1]),0.0,self.vmtkRenderer.Renderer)
+        # We treat result as a boolean value (which can also be nonetype if a selection outside the render windw is made)
+        # If the value of result is False or None, then an invalid actor was selected in the scene. 
         if result is None:
             return
         elif result == 0:
             return
-        self.PickedActor = picker.GetActor()
-        
-        self.PickedActor.GetProperty().SetColor(1.0, 0.0, 0.0)
-        self.PickedActor.GetProperty().SetDiffuse(1.0)
-        self.PickedActor.GetProperty().SetSpecular(0.2)
-        
-        self.PickedPoly = self.PickedActor.GetMapper().GetInputAsDataSet()
-        self._OutputPolyDataList.append(self.PickedPoly)  
-        return
 
+        # find the value of "RegionId" stored in the vtkPolyData PointData array. This is a unique number
+        # which is generated for each connected surface point by the allRegionConnectivity 
+        # vtkPolyDataConnectivityFilter function
+        pickedPointId = picker.GetPointId()
+        self.CurrentPickedActor = picker.GetActor()
+        self.CurrentPickedPolyData = self.CurrentPickedActor.GetMapper().GetInputAsDataSet()
+        regionId = self.CurrentPickedPolyData.GetPointData().GetArray('RegionId').GetValue(pickedPointId)
         
-    def InitializeSeeds(self):
-        self.PickedSeedIds.Initialize()
-        self.PickedSeeds.Initialize()
-        seedPoints = vtk.vtkPoints()
-        self.PickedSeeds.SetPoints(seedPoints)
+        # extract only the desired region Id from the dataset
+        isolatedFilter = vtk.vtkPolyDataConnectivityFilter()
+        isolatedFilter.SetInputData(self.CurrentPickedPolyData)
+        isolatedFilter.SetExtractionModeToSpecifiedRegions()
+        isolatedFilter.AddSpecifiedRegion(regionId)
+
+        # remove all points not in the extracted cells
+        cleanedFilter = vtk.vtkCleanPolyData()
+        cleanedFilter.SetInputConnection(isolatedFilter.GetOutputPort())
+        cleanedFilter.Update()
+
+        outputSurface = cleanedFilter.GetOutput()
+
+        # create a new mapper and actor for this extracted surface object
+        mapper = vtk.vtkPolyDataMapper()
+        mapper.SetInputData(outputSurface)
+        mapper.ScalarVisibilityOff()
+        mapper.Update()
+
+        actor = vtk.vtkActor()
+        actor.SetMapper(mapper)
+        actor.GetProperty().SetColor(1.0, 0.0, 0.0) # red color
+        actor.GetProperty().SetDiffuse(1.0)
+        actor.GetProperty().SetSpecular(0.2)
+        actor.GetProperty().SetRepresentationToSurface()
+        actor.GetProperty().EdgeVisibilityOff()
+
+        # place the extracted surface object's actor instance in the necessary render windows
+        # and lists to log it's selection        
+        self.PastPickedActorList.append(actor)
+        self._OutputPolyDataList.append(outputSurface)
+        self.vmtkRenderer.Renderer.AddActor(actor)
+        self.vmtkRenderer.RenderWindow.Render()
+        return
 
 
     def Execute(self):
@@ -88,12 +122,12 @@ class vmtkPickPointSeedSelector(object):
 
         self.InputInfo('Please position the mouse and press space to add target points, \'u\' to undo\n')
 
+        # this is necessary to set up the async watch loop. 
         any = 0
         while any == 0:
-            self.InitializeSeeds()
             self.vmtkRenderer.Render()
             try:
-                any = self.PickedPoly
+                any = self.CurrentPickedPolyData
             except AttributeError:
                 any = None
 
@@ -136,38 +170,34 @@ class vmtkSurfaceConnectivitySelector(pypes.pypeScript):
         if self.SeedSelector:
             pass
         else:
+            # initialize the seed selection method
             self.SeedSelector = vmtkPickPointSeedSelector()
             self.SeedSelector.vmtkRenderer = self.vmtkRenderer
             self.SeedSelector.Script = self
         
-        connectiv = vtk.vtkPolyDataConnectivityFilter()
-        connectiv.SetInputData(self.Surface)
-        connectiv.SetExtractionModeToAllRegions()
-        connectiv.ColorRegionsOn()
-        connectiv.Update()
-        numRegions = connectiv.GetNumberOfExtractedRegions()
+        # label all non-connected regions in an input surface. 
+        allRegionConnectivity = vtk.vtkPolyDataConnectivityFilter()
+        allRegionConnectivity.SetInputData(self.Surface)
+        allRegionConnectivity.SetExtractionModeToAllRegions()
+        allRegionConnectivity.ColorRegionsOn()
+        allRegionConnectivity.Update()
 
-        for surfaceId in range(numRegions):
-            connectiv2 = vtk.vtkPolyDataConnectivityFilter()
-            connectiv2.SetExtractionModeToSpecifiedRegions()
-            connectiv2.SetInputData(connectiv.GetOutput())
-            connectiv2.AddSpecifiedRegion(surfaceId)
-            connectiv2.Update()
-            surf = connectiv2.GetOutput()
-            
-            mapper = vtk.vtkPolyDataMapper()
-            mapper.SetInputData(surf)
-            mapper.ScalarVisibilityOff()
-            actor = vtk.vtkActor()
-            actor.SetMapper(mapper)
+        # create the actor/mapper which renders the labeled input surface
+        mapper = vtk.vtkPolyDataMapper()
+        mapper.SetInputData(allRegionConnectivity.GetOutput())
+        mapper.ScalarVisibilityOff()
+        mapper.Update()
 
-            actor.GetProperty().SetColor(0.7, 0.7, 0.7)
-            actor.GetProperty().SetDiffuse(0.9)
-            actor.GetProperty().SetSpecular(0.2)
-            actor.GetProperty().SetRepresentationToSurface()
-            actor.GetProperty().EdgeVisibilityOff()
+        actor = vtk.vtkActor()
+        actor.SetMapper(mapper)
+        actor.GetProperty().SetColor(0.7, 0.7, 0.7) # standard gray color
+        actor.GetProperty().SetDiffuse(0.9)
+        actor.GetProperty().SetSpecular(0.2)
+        actor.GetProperty().SetRepresentationToSurface()
+        actor.GetProperty().EdgeVisibilityOff()
 
-            self.vmtkRenderer.Renderer.AddActor(actor)
+        # add this as the main actor to the scene (the first think you see)
+        self.vmtkRenderer.Renderer.AddActor(actor)
 
         if self.SeedSelector:
             self.SeedSelector.InputInfo = self.InputInfo
@@ -175,12 +205,23 @@ class vmtkSurfaceConnectivitySelector(pypes.pypeScript):
             self.SeedSelector.OutputText = self.OutputText
             self.SeedSelector.PrintError = self.PrintError
             self.SeedSelector.PrintLog = self.PrintLog
+            # until SeedSelector releases control of execution, the program will not
+            # proceed past the next line. SeedSelector handles all user interaction and
+            # modification/logging of actor properties & region Ids
             self.SeedSelector.Execute()
 
+            # we store each extracted region as individual vtkPolyData objects within a list.
+            # They are combined into one dataset below.
             polyDataAppender = vtk.vtkAppendPolyData()
             for outputSurfacePolyData in self.SeedSelector._OutputPolyDataList:
                 polyDataAppender.AddInputData(outputSurfacePolyData)
-            polyDataAppender.Update()    
+
+            # Even though we clean the selection in the SeedSelection, this is necessary because it
+            # may be possible for a user to select the same polydata object twice. If this is not cleaned,
+            # a double selection may mess with further post processing scripts. 
+            polyDataCleaner = vtk.vtkCleanPolyData()
+            polyDataCleaner.SetInputConnection(polyDataAppender.GetOutputPort())
+            polyDataCleaner.Update()
 
             self.Surface = polyDataAppender.GetOutput()
 
