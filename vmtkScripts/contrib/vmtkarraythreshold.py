fd894cf8039043e79adf1e7e742cb2b5691c5773
Marco Fedele <fedele.marco@gmail.com>
2019-07-07 16:26:46 +0200

    Add 25 new contrib/vmtkScripts scripts for cardiac mesh processing
    Each of these is a new script, registered in CMakeLists.txt and
    vmtkcontribscripts.py/vmtkscripts.py. Most are pure additions; five also
    touch another script this same author had added earlier in this branch
    (noted below), as a small follow-up tweak made while building the new one:
    
    - vmtkarraythreshold: generalized thresholding on float values, point-data
      or cell-data arrays
    - vmtksurfaceimplicitdistance / vmtkmeshimplicitdistance: signed/unsigned
      distance from a reference surface, with optional binary/cell-data output
      and distance threshold
    - vmtksurfacemodeller2: implicit-distance image from a surface, with
      configurable bounds, spacing and threshold
    - vmtksurfacemodellerbooleanoperation: boolean operation between surfaces
      by transforming them into images
    - vmtksurfacetagger: tag surface regions via array clipping, connectivity,
      constant value, interactive drawing, or harmonic-extension-based methods
    - vmtksurfaceconnector: join two surface rings from one or two input
      surfaces
    - vmtksurfacethickness: compute surface thickness, including
      bi-ventricular geometries
    - vmtksurfaceharmonicsections: divide a surface into harmonic sections by
      solving the heat equation
    - vmtksurfacethickening: thicken or thin a surface below/above a
      threshold, with optional id exclusion
    - vmtksurfaceharmonicextension: harmonically extend a boundary vector
      field over a surface
    - vmtkmeshtethex: convert tetrahedral meshes to hexahedral (TetHex)
    - vmtkmeshinfo: print info about cell numbers and cell types in a mesh
    - vmtksurfacewarpbyvector: warp a surface by a vector field
    - vmtksurfaceharmonicsolver: solve a Laplace/harmonic problem on a surface
      with configurable boundary conditions and interactive display
    - vmtksurfacefeatureedges: vtkFeatureEdges as a script
    - vmtkmeshconnector: connect two mesh regions (same or different input
      meshes) into a merged volumetric mesh
    - vmtkentityextractor: extract a subset of entity ids from a
      surface/mesh, optionally returning the deleted part
    - vmtkmeshappend: merge a set of meshes into a unique one
    - vmtksurfacecoords: add coordinates as a point-data array on a surface
    - vmtkmeshcoords: mesh equivalent of vmtksurfacecoords; also tweaks
      vmtksurfacecoords
    - vmtkmeshinternalfacescleaner: delete internal 2D cells from a
      vtkUnstructuredGrid; also tweaks vmtkentityextractor
    - vmtksurfacebooleanconnector: connect two surfaces via boolean union
      with optional remeshing; also tweaks vmtksurfacetagger
    - vmtksurfaceharmonicconnector: connect a surface to a reference ring
      geometry via the harmonic solver, with optional ICP preprocessing and
      BC/display options; also tweaks vmtksurfacewarpbyvector
    - vmtkmeshwarpbyvector: warp a mesh by a vector field; also tweaks
      vmtksurfacewarpbyvector
    
    Co-authored-by: Ivan Fumagalli <ivan.fumagalli@polimi.it>
    Co-authored-by: Michele Bucelli <michele.bucelli@polimi.it>

---
+++ vmtkScripts/contrib/vmtkarraythreshold.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+from __future__ import absolute_import #NEEDS TO STAY AS TOP LEVEL MODULE FOR Py2-3 COMPATIBILITY
+import sys
+import math
+import vtk
+from vmtk import pypes
+from vmtk import vmtkscripts
+
+
+class vmtkArrayThreshold(pypes.pypeScript):
+    def __init__(self):
+        pypes.pypeScript.__init__(self)
+
+        self.SetScriptName('vmtkarraythreshold')
+        self.SetScriptDoc('Extract part of surface or mesh with a point-data or cell-data array between given thresholds.')
+
+        self.Surface = None
+        self.Mesh = None
+        self.ArrayName = 'CellEntityIds'
+        self.CellData = 1
+        self.LowThreshold = 0
+        self.HighThreshold = 1
+
+        # Member info: name, cmdlinename, typename, num, default, desc[, defaultpipetoscript]
+        self.SetInputMembers([
+                ['Surface', 'isurface', 'vtkPolyData', 1, '',
+                 'the input surface', 'vmtksurfacereader'],
+                ['Mesh', 'imesh', 'vtkUnstructuredGrid', 1, '',
+                 'the input mesh', 'vmtkmeshreader'],
+                ['ArrayName', 'array', 'str', 1, '',
+                 'array to process'],
+                ['CellData','celldata','bool',1,'','toggle processing a cell-data or a point-data array'],
+                ['LowThreshold', 'lowthreshold', 'float', 1, '',
+                 'lower threshold for surface filtering', ''],
+                ['HighThreshold', 'highthreshold', 'float', 1, '',
+                 'higher threshold for surface filtering', ''],
+                ])
+        self.SetOutputMembers([
+                ['Surface', 'osurface', 'vtkPolyData', 1, '',
+                 'the output surface', 'vmtksurfacewriter'],
+                ['Mesh', 'omesh', 'vtkUnstructuredGrid', 1, '',
+                 'the output mesh', 'vmtkmeshwriter'],
+                ['ArrayName', 'array', 'str', 1, '',
+                 'processed array name'],
+                ])
+
+    def Execute(self):
+        if self.Surface == None and self.Mesh == None:
+            self.PrintError('Error: No Surface or Mesh.')
+        if self.Surface != None and self.Mesh != None:
+            self.PrintError('Error: Both Surface and Mesh, expecting only one.')
+
+        input = self.Surface or self.Mesh
+
+        th = vtk.vtkThreshold()
+        th.SetInputData(input)
+        if self.CellData:
+            th.SetInputArrayToProcess(0, 0, 0, 1, self.ArrayName)
+        else:
+            th.SetInputArrayToProcess(0, 0, 0, 0, self.ArrayName)
+        th.SetLowerThreshold(self.LowThreshold)
+        th.SetUpperThreshold(self.HighThreshold)
+        th.SetThresholdFunction(vtk.vtkThreshold.THRESHOLD_BETWEEN)
+        th.Update()
+
+        if self.Mesh != None:
+            self.Mesh = th.GetOutput()
+        else:
+            assert self.Surface != None
+            gf = vtk.vtkGeometryFilter()
+            gf.SetInputConnection(th.GetOutputPort())
+            gf.Update()
+            self.Surface = gf.GetOutput()
+
+
+if __name__ == '__main__':
+    main = pypes.pypeMain()
+    main.Arguments = sys.argv
+    main.Execute()
---
+++ vmtkScripts/vmtkmeshcoords.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+## Program:   VMTK
+## Module:    $RCSfile: vmtkmeshappend.py,v $
+## Language:  Python
+## Date:      $Date: 2021/09/16 09:53:14 $
+## Version:   $Revision: 1.8 $
+
+##   Copyright (c) Luca Antiga, David Steinman. All rights reserved.
+##   See LICENSE file for details.
+
+##      This software is distributed WITHOUT ANY WARRANTY; without even
+##      the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+##      PURPOSE.  See the above copyright notices for more information.
+
+## Note: this class was contributed by
+##       Marco Fedele (marco.fedele@polimi.it)
+##       Politecnico di Milano
+
+from __future__ import absolute_import #NEEDS TO STAY AS TOP LEVEL MODULE FOR Py2-3 COMPATIBILITY
+import sys
+import math
+import vtk
+
+from vmtk import vtkvmtk
+from vmtk import pypes
+
+
+class vmtkMeshCoords(pypes.pypeScript):
+
+    def __init__(self):
+
+        pypes.pypeScript.__init__(self)
+
+        self.Mesh = None
+
+        self.SetScriptName('vmtkMeshcoords')
+        self.SetScriptDoc('add coordinates as point data array on the surface')
+        self.SetInputMembers([
+            ['Mesh','i','vtkUnstructuredGrid',1,'','the input surface','vmtkmeshreader'],
+            ])
+        self.SetOutputMembers([
+            ['Mesh','o','vtkUnstructuredGrid',1,'','the output surface','vmtkmeshwriter']
+            ])
+
+    def Execute(self):
+        from vmtk import vmtkscripts
+        if self.Mesh == None:
+            self.PrintError('Error: No input surface.')
+
+        vmtkscripts.vmtkSurfaceCoords().CreateCoords(self.Mesh)
+
+
+
+if __name__=='__main__':
+
+    main = pypes.pypeMain()
+    main.Arguments = sys.argv
+    main.Execute()
