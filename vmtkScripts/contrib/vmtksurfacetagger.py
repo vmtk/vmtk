c0c04831f4140c9abbab6714aac424983308b54f
Marco Fedele <fedele.marco@gmail.com>
2019-07-11 19:41:51 +0200

    starting implementing a new tagging method based on harmonic extension

--- vmtkScripts/contrib/vmtksurfacetagger.py
+++ vmtkScripts/contrib/vmtksurfacetagger.py
@@ -20,6 +20,7 @@
 from __future__ import absolute_import #NEEDS TO STAY AS TOP LEVEL MODULE FOR Py2-3 COMPATIBILITY
 import vtk
 import sys
+import math
 
 from vmtk import vmtkrenderer
 from vmtk import pypes
@@ -36,7 +37,8 @@ class vmtkSurfaceTagger(pypes.pypeScript):
         self.CellEntityIdsArray = None
         self.ArrayName = None
         self.Array = None
-        self.Value = 0.0
+        self.Value = None
+        self.Range = None
         self.InsideTag = 2
         self.OutsideTag = 1
         self.OverwriteOutsideTag = 0
@@ -45,19 +47,22 @@ class vmtkSurfaceTagger(pypes.pypeScript):
         self.TagSmallestRegion = 1
         self.CleanOutput = 1
         self.PrintTags = 1
+        self.HarmonicRadius = 1.0
 
         self.SetScriptName('vmtksurfacetagger')
         self.SetScriptDoc('tag a surface exploiting an array defined on it')
         self.SetInputMembers([
             ['Surface','i','vtkPolyData',1,'','the input surface','vmtksurfacereader'],
-            ['Method','method','str',1,'["cliparray","array","connectivity","constant","drawing"]','tagging method (cliparray: exploit an array to clip the surface at a certain value tagging the two parts; array: the same of cliparray, but without clipping the original triangles; connectivity: given an already tagged surface, tag disconnected part of each input tag; constant: assign a constant tag to the input surface; drawing: interactive drawing a region)'],
+            ['Method','method','str',1,'["cliparray","array","harmonic","connectivity","constant","drawing"]','tagging method (cliparray: exploit an array to clip the surface at a certain value tagging the two parts; array: the same of cliparray, but without clipping the original triangles; harmonic: the same of array, but trying to moving harmonically the original points toward the tag value; connectivity: given an already tagged surface, tag disconnected part of each input tag; constant: assign a constant tag to the input surface; drawing: interactive drawing a region)'],
             ['CellEntityIdsArrayName','entityidsarray','str',1,'','name of the array where the tags are stored'],
             ['ArrayName','array','str',1,'','name of the array with which to define the boundary between tags'],
             ['Value','value','float',1,'','scalar value of the array identifying the boundary between tags'],
+            ['Range','range','float',2,'','range scalar values of the array identifying the region for the new tag (alternative to value)'],
+            ['HarmonicRadius','harmonicradius','float',1,'','buffer zone radius for the harmonic method'],
             ['InsideTag','inside','int',1,'','tag of the inside region (i.e. where the Array is lower than Value; used also in case of "constant" method)'],
             ['OverwriteOutsideTag','overwriteoutside','bool',1,'','overwrite outside value also when the CellEntityIdsArray already exists in the input surface'],
             ['OutsideTag','outside','int',1,'','tag of the outside region (i.e. where the Array is greater than Value)'],
-            ['InsideOut','insideout','bool',1,'','toggle switching inside and outside tags ("cliparray" and "array" methods only)'],
+            ['InsideOut','insideout','bool',1,'','toggle switching inside and outside tags ("cliparray" and "array" methods, only when specifying value and not range)'],
             ['ConnectivityOffset','offset','int',1,'','offset added to the entityidsarray of each disconnected parts of each input tag (connectivity only)'],
             ['TagSmallestRegion','tagsmallestregion','bool',1,'','toggle tagging the smallest or the largest region (drawing only)'],
             ['CleanOutput','cleanoutput','bool',1,'','toggle cleaning the unused points'],
@@ -79,7 +84,7 @@ class vmtkSurfaceTagger(pypes.pypeScript):
 
 
 
-    def ClipArrayTagger(self):
+    def ClipArrayTagger(self,onlyRing=False):
 
         from vmtk import vmtkscripts
 
@@ -94,42 +99,138 @@ class vmtkSurfaceTagger(pypes.pypeScript):
 
         clipper.Execute()
 
-        insideSurface = clipper.Surface
-        outsideSurface = clipper.ClippedSurface
+        if onlyRing:
+            return clipper.CutLines
+        else:
+            insideSurface = clipper.Surface
+            outsideSurface = clipper.ClippedSurface
 
-        # change values of the inside tags
-        insideCellEntityIdsArray = insideSurface.GetCellData().GetArray( self.CellEntityIdsArrayName )
-        outsideCellEntityIdsArray = outsideSurface.GetCellData().GetArray( self.CellEntityIdsArrayName )
+            # change values of the inside tags
+            insideCellEntityIdsArray = insideSurface.GetCellData().GetArray( self.CellEntityIdsArrayName )
+            outsideCellEntityIdsArray = outsideSurface.GetCellData().GetArray( self.CellEntityIdsArrayName )
 
-        insideCellEntityIdsArray.FillComponent(0,self.InsideTag)
+            insideCellEntityIdsArray.FillComponent(0,self.InsideTag)
 
-        # merge the inside and the outside surfaces
-        mergeSurface = vtk.vtkAppendPolyData()
-        mergeSurface.AddInputData(insideSurface)
-        mergeSurface.AddInputData(outsideSurface)
-        mergeSurface.Update()
+            # merge the inside and the outside surfaces
+            mergeSurface = vtk.vtkAppendPolyData()
+            mergeSurface.AddInputData(insideSurface)
+            mergeSurface.AddInputData(outsideSurface)
+            mergeSurface.Update()
 
-        self.Surface = mergeSurface.GetOutput()
-        self.CellEntityIdsArray = self.Surface.GetCellData().GetArray(self.CellEntityIdsArrayName)
+            self.Surface = mergeSurface.GetOutput()
+            self.CellEntityIdsArray = self.Surface.GetCellData().GetArray(self.CellEntityIdsArrayName)
 
 
 
-    def ArrayTagger(self):
+    def ArrayTagger(self,surface=None,arrayName=None,insideTag=None,value=None,value2=None,):
+        if surface == None:
+            surface = self.Surface
+        if arrayName == None:
+            arrayName = self.ArrayName
+        if insideTag == None:
+            insideTag = self.InsideTag
+        if value == None:
+            value = self.Value
         pointsToCells = vtk.vtkPointDataToCellData()
-        pointsToCells.SetInputData(self.Surface)
+        pointsToCells.SetInputData(surface)
         pointsToCells.PassPointDataOn()
         pointsToCells.Update()
-        self.Surface = pointsToCells.GetPolyDataOutput()
-        self.CellEntityIdsArray = self.Surface.GetCellData().GetArray(self.CellEntityIdsArrayName)
-        cellArray = self.Surface.GetCellData().GetArray(self.ArrayName)
-        if self.InsideOut:
-            for i in range(self.Surface.GetNumberOfCells()):
-                if cellArray.GetValue(i) > self.Value:
-                    self.CellEntityIdsArray.SetValue(i,self.InsideTag)
+        surface = pointsToCells.GetPolyDataOutput()
+        cellEntityIdsArray = surface.GetCellData().GetArray(self.CellEntityIdsArrayName)
+        cellArray = surface.GetCellData().GetArray(arrayName)
+        if value2 == None:
+            if self.InsideOut:
+                for i in range(surface.GetNumberOfCells()):
+                    if cellArray.GetValue(i) > value:
+                        cellEntityIdsArray.SetValue(i,insideTag)
+            else:
+                for i in range(surface.GetNumberOfCells()):
+                    if cellArray.GetValue(i) < value:
+                        cellEntityIdsArray.SetValue(i,insideTag)
         else:
-            for i in range(self.Surface.GetNumberOfCells()):
-                if cellArray.GetValue(i) < self.Value:
-                    self.CellEntityIdsArray.SetValue(i,self.InsideTag)
+            for i in range(surface.GetNumberOfCells()):
+                if cellArray.GetValue(i) > value and cellArray.GetValue(i) < value2:
+                    cellEntityIdsArray.SetValue(i,insideTag)
+
+        return surface
+
+
+    def HarmonicTagger(self):
+        from vmtk import vmtkscripts
+        from vmtk import vmtkcontribscripts
+
+        tags = set()
+        for i in range(self.Surface.GetNumberOfCells()):
+            tags.add(self.CellEntityIdsArray.GetComponent(i,0))
+        tags = sorted(tags)
+
+        # use clip-array method only to extract the ring
+        preciseRing = self.ClipArrayTagger(True)
+
+        # tag using array method
+        self.ArrayTagger()
+
+        # warp the points on the zig-zag ring till the precise ring and harmonically extend the warp
+        th = vmtkcontribscripts.vmtkThreshold()
+        th.Surface = self.Surface
+        th.CellEntityIdsArrayName = self.CellEntityIdsArrayName
+        th.LowThreshold = self.InsideTag
+        th.HighThreshold = self.InsideTag
+        th.Execute()
+        newTag = th.Surface
+
+        featureEdges = vtk.vtkFeatureEdges()
+        featureEdges.BoundaryEdgesOn()
+        featureEdges.FeatureEdgesOff()
+        featureEdges.NonManifoldEdgesOff()
+        featureEdges.ManifoldEdgesOff()
+        featureEdges.ColoringOff()
+        featureEdges.SetInputData(newTag)
+        featureEdges.CreateDefaultLocator()
+        featureEdges.Update()
+        zigZagRing = featureEdges.GetOutput()
+
+        surfaceDistance = vmtkscripts.vmtkSurfaceDistance()
+        surfaceDistance.Surface = zigZagRing
+        surfaceDistance.ReferenceSurface = preciseRing
+        surfaceDistance.DistanceVectorsArrayName = 'PreciseRingDistance'
+        surfaceDistance.Execute()
+        zigZagRing = surfaceDistance.Surface
+
+        passArray = vtk.vtkPassArrays()
+        passArray.SetInputData(zigZagRing)
+        passArray.AddPointDataArray('PreciseRingDistance')
+        passArray.Update()
+        zigZagRing = passArray.GetOutput()
+
+        proj = vmtkscripts.vmtkSurfaceProjection()
+        proj.Surface = newTag
+        proj.ReferenceSurface = zigZagRing
+        proj.Execute()
+        newTag = proj.Surface
+
+        surfaceDistance2 = vmtkscripts.vmtkSurfaceDistance()
+        surfaceDistance2.Surface = newTag
+        surfaceDistance2.ReferenceSurface = zigZagRing
+        surfaceDistance2.DistanceArrayName = 'ZigZagRingDistance'
+        surfaceDistance2.Execute()
+        newTag = surfaceDistance2.Surface
+
+        # adding a temporary tag
+        newTag = self.ArrayTagger(newTag,'ZigZagRingDistance',20,self.HarmonicRadius)
+        # self.Surface = self.ArrayTagger(self.Surface,'ZigZagRingDistance',21,-self.HarmonicRadius,0.0)
+
+        harmonicExtension = vmtkcontribscripts.vmtkSurfaceHarmonicExtension()
+        harmonicExtension.Surface = newTag
+        harmonicExtension.InputArrayName = 'PreciseRingDistance'
+        harmonicExtension.OutputArrayName = 'WarpTagging'
+        harmonicExtension.ProjectionMethod = 'none'
+        harmonicExtension.SmoothProjection = 0
+        harmonicExtension.RangeIds = [self.InsideTag, 20]
+        harmonicExtension.OutletIds = [self.InsideTag]
+        harmonicExtension.BoundaryConditions = [0.0, 1.0]
+        harmonicExtension.Execute()
+        self.Surface = harmonicExtension.Surface
 
 
 
@@ -214,10 +315,22 @@ class vmtkSurfaceTagger(pypes.pypeScript):
             self.Surface.GetCellData().AddArray(self.CellEntityIdsArray)
             self.CellEntityIdsArray.FillComponent(0,self.OutsideTag)
 
+        if self.Method in ['cliparray','array','harmonic']:
+            if self.Value == None and self.Range == None:
+                self.PrintError("This method need the definition of a value or a range")
+            elif self.Range == None:
+                if self.InsideOut:
+                    self.Range = [self.Value, math.inf]
+                else:
+                    self.Range = [-math.inf, self.Value]
+                print("range: ",self.Range)
+
         if self.Method == 'cliparray':
             self.ClipArrayTagger()
         elif self.Method == 'array':
             self.ArrayTagger()
+        elif self.Method == 'harmonic':
+            self.HarmonicTagger()
         elif self.Method == 'connectivity':
             self.ConnectivityTagger()
         elif self.Method == 'constant':
