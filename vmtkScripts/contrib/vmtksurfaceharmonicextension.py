011c693d25eac92fcf13b538caa69dc68b2d1130
Marco Fedele <marco.fedele@polimi.it>
2020-04-09 18:45:24 +0200

    refactoring of harmonic extension script using the new harmonic solver

--- vmtkScripts/contrib/vmtksurfaceharmonicextension.py
+++ vmtkScripts/contrib/vmtksurfaceharmonicextension.py
@@ -14,7 +14,7 @@
 ##      PURPOSE.  See the above copyright notices for more information.
 
 ## Note: this class was contributed by 
-##       Elena Faggiano (elena.faggiano@gmail.com)
+##       Marco Fedele (marco.fedele@polimi.it)
 ##       Politecnico di Milano
 
 from __future__ import absolute_import #NEEDS TO STAY AS TOP LEVEL MODULE FOR Py2-3 COMPATIBILITY
@@ -36,43 +36,36 @@ class vmtkSurfaceHarmonicExtension(pypes.pypeScript):
         self.Surface = None
         self.InputArrayName = 'Displacement'
         self.OutputArrayName = 'DisplacementOut'
-        self.ArrayIndexes = []
-        self.InputArray = None
         self.OutputArray = None
-        self.RangeIds = None
-        self.OutletIds = []
-        self.BoundaryConditions = []
-        self.MethodX = "harmonic"
-        self.MethodY = "harmonic"
-        self.MethodZ = "harmonic"
-        self.ProjectionMethod = "surface"
-        self.SmoothProjection = 0
-        self.SmoothingConnexity = 1
-        self.SmoothingRelaxation = 1.0
-        self.SmoothingIterations = 1
+        self.ArraySuffixes = []
+
+        self.Ids = set()
+        self.ExtensionIds = []
+        self.ExcludeIds = []
+        self.ExcludeIdsForBCs = []
+
+        self.UseNullDirichletBCs = 1
+
         self.CellEntityIdsArrayName = 'CellEntityIds'
         self.CellEntityIdsArray = None
 
+        self.Display = 0
+        self.vmtkRenderer = None
+
 
         self.SetScriptName('vmtksurfaceharmonicextension')
-        self.SetScriptDoc('extend an input vector harmonically on a surface')
+        self.SetScriptDoc('extend an input array harmonically on a surface; the extension takes effect on a subset of the input surface selected using  entity ids; the output array remains the same of the input one outside the extension domain and the values of this array at the boundary rings of the excluded domain are used as Dirichlet BCs for the harmonic extension')
         self.SetInputMembers([
             ['Surface','i','vtkPolyData',1,'','the input surface','vmtksurfacereader'],
             ['InputArrayName','inputarray','str',1,'','input array to be extended on some tags'],
             ['OutputArrayName','outputarray','str',1,'','output array name'],
-            ['ArrayIndexes','arrayindexes','str',-1,'','set of indexes to be added to input/output arrays in order to process a set of arrays instead only one (if not set, only a single inputarray is processed)'],
-            ['RangeIds','rangeids','int',2,'','range of ids where to extend the input array'],
-            ['OutletIds','outletids','int',-1,'','ids where to impose the heat equation bcs (these ids must be in rangeids)'],
-            ['BoundaryConditions','bcs','float',-1,'','list of bcs for the harmonic extension outlets, ordered as boundary id'],
-            ['MethodX','methodx','str',1,'["harmonic","projection","meanring"]','possible extensions methods'],
-            ['MethodY','methody','str',1,'["harmonic","projection","meanring"]','possible extensions methods'],
-            ['MethodZ','methodz','str',1,'["harmonic","projection","meanring"]','possible extensions methods'],
-            ['ProjectionMethod','projectionmethod','str',1,'["surface","ring","none"]','possible projection methods'],
-            ['SmoothProjection','smoothprojection','bool',1,'','toggle smoothing of the projected array on the harmonic surface'],
-            ['SmoothingConnexity','connexity','int',1,'(1,2)','patch connexity considered in the smoothing procedure'],
-            ['SmoothingRelaxation','relaxation','float',1,'(0.0,1.0)','relaxation factor'],
-            ['SmoothingIterations','iterations','int',1,'','number of smoothing iterations'],
+            ['ArraySuffixes','arraysuffixes','str',-1,'','set of suffixes for the input/output arrays in order to process a set of arrays instead only one (if not set, only a single inputarray is processed)'],
+            ['ExtensionIds','extensionids','int',-1,'','entity ids of the surface identifying the extension domain, i.e. where to extend the input array'],
+            ['ExcludeIdsForBCs','excludeidsforbcs','int',-1,'','subset of extension domain to be excluded only for the boundary rings definition, where to set the Dirichlet BCs; this option only takes effect if "UseNullDirichletBCs" is true'],
+            ['UseNullDirichletBCs','dirichletbcs','bool',1,'','toggle imposing homogeneous Dirichlet BCs on the rings of the extension domain; "ExcludeIdsForBCs" option can be exploited to define these rings'],
             ['CellEntityIdsArrayName', 'entityidsarray', 'str', 1, '','name of the array where entity ids have been stored'],
+            ['Display','display','bool',1,'','toggle rendering'],
+            ['vmtkRenderer','renderer','vmtkRenderer',1,'','external renderer']
             ])
         self.SetOutputMembers([
             ['Surface','o','vtkPolyData',1,'','the output surface','vmtksurfacewriter']
@@ -89,202 +82,60 @@ class vmtkSurfaceHarmonicExtension(pypes.pypeScript):
 
         self.CellEntityIdsArray = self.Surface.GetCellData().GetArray(self.CellEntityIdsArrayName)
 
-        tags = set()
         for i in range(self.Surface.GetNumberOfCells()):
-            tags.add(self.CellEntityIdsArray.GetComponent(i,0))
-        tags = sorted(tags)
-        self.PrintLog('Tags of the input surface: '+str(tags))
+            self.Ids.add(int(self.CellEntityIdsArray.GetComponent(i,0)))
+        self.Ids = sorted(self.Ids)
+
+        for item in self.Ids:
+            if item not in self.ExtensionIds:
+                self.ExcludeIds.append(item)
+
 
         # add indexes to input/output arrays
         inputArrayNames = []
         outputArrayNames = []
-        if self.ArrayIndexes == []:
+        if self.ArraySuffixes == []:
             inputArrayNames.append(self.InputArrayName)
             outputArrayNames.append(self.OutputArrayName)
         else:
-            for index in self.ArrayIndexes:
-                inputArrayNames.append(self.InputArrayName+index)
-                outputArrayNames.append(self.OutputArrayName+index)
+            for suffix in self.ArraySuffixes:
+                inputArrayNames.append(self.InputArrayName+suffix)
+                outputArrayNames.append(self.OutputArrayName+suffix)
 
-        self.PrintLog("Input arrays: "+str(inputArrayNames))
+        self.PrintLog("Input arrays:  "+str(inputArrayNames))
         self.PrintLog("Output arrays: "+str(outputArrayNames))
 
+        for i in range(len(inputArrayNames)):
+            inputArray = self.Surface.GetPointData().GetArray(inputArrayNames[i])
+            numberOfComponents = inputArray.GetNumberOfComponents()
 
-        def surfaceThreshold(surface,low,high):
-            th = vmtkcontribscripts.vmtkThreshold()
-            th.Surface = surface
-            th.CellEntityIdsArrayName = self.CellEntityIdsArrayName
-            th.LowThreshold = low
-            th.HighThreshold = high
-            th.Execute()
-            surf = th.Surface
-            return surf
-
-        def surfaceAppend(surface1,surface2):
-            if surface1 == None:
-                surf = surface2
-            elif surface2 == None:
-                surf = surface1
+            harmonicSolver = vmtkcontribscripts.vmtkSurfaceHarmonicSolver()
+            harmonicSolver.Surface = self.Surface
+            harmonicSolver.Interactive = False
+            if numberOfComponents == 3:
+                harmonicSolver.VectorialEq = True
             else:
-                a = vmtkscripts.vmtkSurfaceAppend()
-                a.Surface = surface1
-                a.Surface2 = surface2
-                a.Execute()
-                surf = a.Surface 
-                tr = vmtkscripts.vmtkSurfaceTriangle()
-                tr.Surface = surf
-                tr.Execute()
-                surf = tr.Surface
-            return surf
-
-        def surfaceProjection(isurface,rsurface):
-            proj = vmtkscripts.vmtkSurfaceProjection()
-            proj.Surface = isurface
-            proj.ReferenceSurface = rsurface
-            proj.Execute()
-            return proj.Surface
-
-
-        surfaceHarmonicCaps = None
-        surfaceHarmonicDomain = None
-        surfaceNotProcessed = None
-
-        for item in tags:
-            surfaceTh = surfaceThreshold(self.Surface,item,item)
-            if item <= self.RangeIds[1] and item >= self.RangeIds[0]:
-                if item in self.OutletIds:
-                    surfaceHarmonicCaps = surfaceAppend(surfaceHarmonicCaps,surfaceTh)
-                else:
-                    surfaceHarmonicDomain = surfaceAppend(surfaceHarmonicDomain,surfaceTh)
+                harmonicSolver.VectorialEq = False
+            harmonicSolver.SolutionArrayName = outputArrayNames[i]
+            harmonicSolver.ExcludeIds = self.ExcludeIds
+            harmonicSolver.ExcludeIdsArrayName = inputArrayNames[i]
+            harmonicSolver.ExcludeIdsForBCs = self.ExcludeIdsForBCs
+            if self.UseNullDirichletBCs:
+                harmonicSolver.InitWithZeroDirBCs = True
             else:
-                surfaceNotProcessed = surfaceAppend(surfaceNotProcessed,surfaceTh)
+                harmonicSolver.InitWithZeroDirBCs = False
+            harmonicSolver.CellEntityIdsArrayName = self.CellEntityIdsArrayName
+            harmonicSolver.Display = self.Display
+            harmonicSolver.vmtkRenderer = self.vmtkRenderer
 
+            # extract rings between includedDomain and excludedDomain 
+            [includedDomain, excludedDomain] = harmonicSolver.ExtractDomain(self.Surface,self.ExcludeIds)
+            rings = harmonicSolver.ExtractBoundaries(excludedDomain) # THIS IS TO FIX
+            harmonicSolver.InputRings = rings
+            harmonicSolver.InputRingsBCsArrayName = inputArrayNames[i]
 
-        meanRingX = 0.0
-        meanRingY = 0.0
-        meanRingZ = 0.0
-
-        # if metodo closest point
-        # proietta il valore di InputArray da surfaceNotProcessed a surfaceHarmonic
-        if self.ProjectionMethod == "surface":
-            surfaceHarmonicCaps = surfaceProjection(surfaceHarmonicCaps,surfaceNotProcessed)
-            surfaceHarmonicDomain = surfaceProjection(surfaceHarmonicDomain,surfaceNotProcessed)
-
-        # if metodo ring:
-        # estrai i ring da surfaceNotProcessed
-        # proietta il valore di InputArray nel ring su surfaceHarmonic
-        elif self.ProjectionMethod == "ring":
-            fe = vtk.vtkFeatureEdges()
-            fe.BoundaryEdgesOn()
-            fe.FeatureEdgesOff()
-            fe.NonManifoldEdgesOff()
-            fe.ManifoldEdgesOff()
-            fe.ColoringOff()
-            fe.SetInputData(surfaceNotProcessed)
-            fe.CreateDefaultLocator()
-            fe.Update()
-            rings = fe.GetOutput()
-            surfaceHarmonicCaps = surfaceProjection(surfaceHarmonicCaps,rings)
-            surfaceHarmonicDomain = surfaceProjection(surfaceHarmonicDomain,rings)
-            # compute mean ring
-            numRingPoints = rings.GetNumberOfPoints()
-            meanRingX = []
-            meanRingY = []
-            meanRingZ = []
-            for name in inputArrayNames:
-                ringInputArray = rings.GetPointData().GetArray(name)
-                sumRingX = 0.0
-                sumRingY = 0.0
-                sumRingZ = 0.0
-                for i in range(numRingPoints):
-                    sumRingX = sumRingX + ringInputArray.GetComponent(i,0)
-                    sumRingY = sumRingY + ringInputArray.GetComponent(i,1)
-                    sumRingZ = sumRingZ + ringInputArray.GetComponent(i,2)
-                meanRingX.append( sumRingX / numRingPoints )
-                meanRingY.append( sumRingY / numRingPoints )
-                meanRingZ.append( sumRingZ / numRingPoints )
-
-        else:
-            self.PrintLog("No projection active")
-
-
-        hs = vmtkcontribscripts.vmtkSurfaceHarmonicSections()
-        hs.Surface = surfaceHarmonicDomain
-        hs.ComputeSections = 0
-        hs.BoundaryConditions = self.BoundaryConditions
-        hs.Execute()
-        surfaceHarmonicDomain = hs.SurfaceHarmonic
-
-        if surfaceHarmonicCaps != None:
-            uCaps = vtk.vtkDoubleArray()
-            uCaps.SetNumberOfComponents(1)
-            uCaps.SetNumberOfTuples(surfaceHarmonicCaps.GetNumberOfPoints())
-            uCaps.SetName('HarmonicMappedTemperature')
-            uCaps.FillComponent(0,0.0)
-            surfaceHarmonicCaps.GetPointData().AddArray(uCaps)
-
-        if surfaceNotProcessed != None:
-            uNotProcessed = vtk.vtkDoubleArray()
-            uNotProcessed.SetNumberOfComponents(1)
-            uNotProcessed.SetNumberOfTuples(surfaceNotProcessed.GetNumberOfPoints())
-            uNotProcessed.SetName('HarmonicMappedTemperature')
-            uNotProcessed.FillComponent(0,1.0)
-            surfaceNotProcessed.GetPointData().AddArray(uNotProcessed)
-
-        surfaceHarmonic = surfaceAppend(surfaceHarmonicDomain,surfaceHarmonicCaps)
-
-        if self.SmoothProjection:
-            for name in inputArrayNames:
-                arraySmoothing = vmtkscripts.vmtkSurfaceArraySmoothing()
-                arraySmoothing.Surface = surfaceHarmonic
-                arraySmoothing.SurfaceArrayName = name
-                arraySmoothing.Connexity = self.SmoothingConnexity
-                arraySmoothing.Relaxation = self.SmoothingRelaxation
-                arraySmoothing.Iterations = self.SmoothingIterations
-                arraySmoothing.Execute()
-                surfaceHarmonic = arraySmoothing.Surface
-
-        self.Surface = surfaceAppend(surfaceHarmonic,surfaceNotProcessed)
-
-        u = self.Surface.GetPointData().GetArray('HarmonicMappedTemperature')
-        for i in range(u.GetNumberOfTuples()):
-            if u.GetTuple1(i) < 0.0:
-                u.SetTuple1(i,0.0)
-
-        for j in range(len(inputArrayNames)):
-            inputArray = self.Surface.GetPointData().GetArray(inputArrayNames[j])
-            outputArray = vtk.vtkDoubleArray()
-            outputArray.SetNumberOfComponents(3)
-            outputArray.SetNumberOfTuples(self.Surface.GetNumberOfPoints())
-            outputArray.SetName(outputArrayNames[j])
-
-            for i in range(outputArray.GetNumberOfTuples()):
-                val = inputArray.GetComponent(i,0)
-                if self.MethodX == "harmonic":
-                    val = val * u.GetTuple1(i)
-                elif self.MethodX == "meanring":
-                    val = (val - meanRingX[j]) * u.GetTuple1(i) + meanRingX[j]
-                outputArray.SetComponent(i,0,val)
-
-                val = inputArray.GetComponent(i,1)
-                if self.MethodY == "harmonic":
-                    val = val * u.GetTuple1(i)
-                elif self.MethodY == "meanring":
-                    val = (val - meanRingY[j]) * u.GetTuple1(i) + meanRingY[j]
-                outputArray.SetComponent(i,1,val)
-
-                val = inputArray.GetComponent(i,2)
-                if self.MethodZ == "harmonic":
-                    val = val * u.GetTuple1(i)
-                elif self.MethodZ == "meanring":
-                    val = (val - meanRingZ[j]) * u.GetTuple1(i) + meanRingZ[j]
-                outputArray.SetComponent(i,2,val)
-                # mean ring 
-                # sposti di: [ val - mean(ring) ] * u + mean(ring)
-            
-            self.Surface.GetPointData().AddArray(outputArray)
-            # self.Surface = surfaceHarmonicDomain
-
+            harmonicSolver.Execute()
+            self.Surface = harmonicSolver.Surface
         
 
 
