da860dbc30d524754f2ca8a36a85abdc59440e5e
Kurt Sansom <kayarre@gmail.com>
2021-11-12 11:48:44 -0600

    fix: work around for calling test fixtures

--- tests/test_vmtkScripts/test_vmtkbifurcationsections.py
+++ tests/test_vmtkScripts/test_vmtkbifurcationsections.py
@@ -50,6 +50,12 @@ def bifur_sections_two_spheres(aorta_centerline_branches ,aorta_surface_branches
     
     return sections.BifurcationSections
 
+@pytest.fixture(scope='module')
+def bifurcation_section_type(request, bifur_sections_one_sphere, bifur_sections_two_spheres):
+    if request.param == "bifur_sections_one_sphere":
+        return bifur_sections_one_sphere
+    elif request.param == "bifur_sections_two_spheres":
+        return bifur_sections_two_spheres
 
 @pytest.mark.parametrize("expectedKey",[
     ('BifurcationSectionGroupIds'),
@@ -89,122 +95,134 @@ def test_expected_number_points_with_two_spheres(bifur_sections_two_spheres):
     assert wrapped_bifur_section.Points.shape == (280, 3)
 
 
-@pytest.mark.parametrize("bifurcation_sections,expectedValue",[
-    (bifur_sections_one_sphere, np.array([0, 2, 3])),
-    (bifur_sections_two_spheres, np.array([0, 2, 3]))
-])
-def test_bifurcation_section_group_ids_correct(aorta_centerline_branches ,aorta_surface_branches,
-                                               bifurcation_sections, expectedValue):
-    wrapped_bifur_section = dsa.WrapDataObject(bifurcation_sections(aorta_centerline_branches ,aorta_surface_branches)) 
+
+
+@pytest.mark.parametrize("bifurcation_section_type,expectedValue",[
+    ("bifur_sections_one_sphere",  np.array([0, 2, 3])),
+    ("bifur_sections_two_spheres",  np.array([0, 2, 3]))
+],
+indirect=["bifurcation_section_type"])
+def test_bifurcation_section_group_ids_correct(
+                                               bifurcation_section_type, expectedValue):
+    wrapped_bifur_section = dsa.WrapDataObject(bifurcation_section_type) 
     assert np.allclose(wrapped_bifur_section.CellData.GetArray('BifurcationSectionGroupIds'), expectedValue) == True
 
 
-@pytest.mark.parametrize("bifurcation_sections,expectedValue",[
-    (bifur_sections_one_sphere, np.array([1, 1, 1])),
-    (bifur_sections_two_spheres, np.array([1, 1, 1]))
-])
-def test_bifurcation_section_bifurcation_group_ids_correct(aorta_centerline_branches ,aorta_surface_branches,
-                                                          bifurcation_sections, expectedValue):
-    wrapped_bifur_section = dsa.WrapDataObject(bifurcation_sections(aorta_centerline_branches ,aorta_surface_branches)) 
+@pytest.mark.parametrize("bifurcation_section_type,expectedValue",[
+    ("bifur_sections_one_sphere",  np.array([1, 1, 1])),
+    ("bifur_sections_two_spheres",  np.array([1, 1, 1]))
+],
+indirect=["bifurcation_section_type"])
+def test_bifurcation_section_bifurcation_group_ids_correct(
+                                                          bifurcation_section_type, expectedValue):
+    wrapped_bifur_section = dsa.WrapDataObject(bifurcation_section_type) 
     assert np.allclose(wrapped_bifur_section.CellData.GetArray('BifurcationSectionBifurcationGroupIds'), expectedValue) == True
 
 
-@pytest.mark.parametrize("bifurcation_sections,expectedValue",[
-    (bifur_sections_one_sphere, np.array([[221.44660291, 147.74956102, 23.43517593],
+@pytest.mark.parametrize("bifurcation_section_type,expectedValue",[
+    ("bifur_sections_one_sphere",  np.array([[221.44660291, 147.74956102, 23.43517593],
                                           [227.15183902, 130.69595192, 23.69846289],
                                           [216.35230991, 130.63763069, 25.46971588]])),
-    (bifur_sections_two_spheres, np.array([[221.36717806, 155.13844109, 22.77446113],
+    ("bifur_sections_two_spheres",  np.array([[221.36717806, 155.13844109, 22.77446113],
                                            [228.43770649, 126.58995359, 24.12527081],
                                            [215.25035964, 126.36271854, 26.00282479]]))
-])
-def test_bifurcation_section_point_correct(aorta_centerline_branches ,aorta_surface_branches,
-                                           bifurcation_sections, expectedValue):
-    wrapped_bifur_section = dsa.WrapDataObject(bifurcation_sections(aorta_centerline_branches ,aorta_surface_branches)) 
+],
+indirect=["bifurcation_section_type"])
+def test_bifurcation_section_point_correct(
+                                           bifurcation_section_type , expectedValue):
+    wrapped_bifur_section = dsa.WrapDataObject(bifurcation_section_type) 
     assert np.allclose(wrapped_bifur_section.CellData.GetArray('BifurcationSectionPoint'), expectedValue) == True
 
 
-@pytest.mark.parametrize("bifurcation_sections,expectedValue",[
-    (bifur_sections_one_sphere, np.array([[ 0.01557548, -0.99202661,  0.12506246],
+@pytest.mark.parametrize("bifurcation_section_type,expectedValue",[
+    ("bifur_sections_one_sphere",  np.array([[ 0.01557548, -0.99202661,  0.12506246],
                                           [ 0.4915811,  -0.860392,    0.1344382 ],
                                           [-0.33415118, -0.94115264,  0.05074142]])),
-    (bifur_sections_two_spheres, np.array( [[-0.1957447, -0.97325556, 0.12023986],
+    ("bifur_sections_two_spheres",  np.array( [[-0.1957447, -0.97325556, 0.12023986],
                                             [ 0.13694085, -0.97859191, 0.15363942],
                                             [-0.15327674, -0.97815097, 0.14045255]]))
-])
-def test_bifurcation_section_normal_correct(aorta_centerline_branches ,aorta_surface_branches,
-                                           bifurcation_sections, expectedValue):
-    wrapped_bifur_section = dsa.WrapDataObject(bifurcation_sections(aorta_centerline_branches ,aorta_surface_branches)) 
+],
+indirect=["bifurcation_section_type"])
+def test_bifurcation_section_normal_correct(
+                                           bifurcation_section_type , expectedValue):
+    wrapped_bifur_section = dsa.WrapDataObject(bifurcation_section_type) 
     assert np.allclose(wrapped_bifur_section.CellData.GetArray('BifurcationSectionNormal'), expectedValue) == True
 
 
-@pytest.mark.parametrize("bifurcation_sections,expectedValue",[
-    (bifur_sections_one_sphere, np.array([177.71780994,   73.02829417,   80.5269763 ])),
-    (bifur_sections_two_spheres, np.array([191.24040267,   63.50445732,   64.12766266]))
-])
-def test_bifurcation_section_area_correct(aorta_centerline_branches ,aorta_surface_branches,
-                                          bifurcation_sections, expectedValue):
-    wrapped_bifur_section = dsa.WrapDataObject(bifurcation_sections(aorta_centerline_branches ,aorta_surface_branches)) 
+@pytest.mark.parametrize("bifurcation_section_type,expectedValue",[
+    ("bifur_sections_one_sphere",  np.array([177.71780994,   73.02829417,   80.5269763 ])),
+    ("bifur_sections_two_spheres",  np.array([191.24040267,   63.50445732,   64.12766266]))
+],
+indirect=["bifurcation_section_type"])
+def test_bifurcation_section_area_correct(
+                                          bifurcation_section_type , expectedValue):
+    wrapped_bifur_section = dsa.WrapDataObject(bifurcation_section_type) 
     assert np.allclose(wrapped_bifur_section.CellData.GetArray('BifurcationSectionArea'), expectedValue) == True
 
 
-@pytest.mark.parametrize("bifurcation_sections,expectedValue",[
-    (bifur_sections_one_sphere, np.array([14.19050112, 9.18671219, 9.5789813 ])),
-    (bifur_sections_two_spheres, np.array([15.12586667, 8.86926931, 8.91067727]))
-])
-def test_bifurcation_section_min_size_correct(aorta_centerline_branches ,aorta_surface_branches,
-                                             bifurcation_sections, expectedValue):
-    wrapped_bifur_section = dsa.WrapDataObject(bifurcation_sections(aorta_centerline_branches ,aorta_surface_branches)) 
+@pytest.mark.parametrize("bifurcation_section_type,expectedValue",[
+    ("bifur_sections_one_sphere",  np.array([14.19050112, 9.18671219, 9.5789813 ])),
+    ("bifur_sections_two_spheres",  np.array([15.12586667, 8.86926931, 8.91067727]))
+],
+indirect=["bifurcation_section_type"])
+def test_bifurcation_section_min_size_correct(
+                                              bifurcation_section_type , expectedValue):
+    wrapped_bifur_section = dsa.WrapDataObject(bifurcation_section_type) 
     assert np.allclose(wrapped_bifur_section.CellData.GetArray('BifurcationSectionMinSize'), expectedValue) == True
 
 
-@pytest.mark.parametrize("bifurcation_sections,expectedValue",[
-    (bifur_sections_one_sphere, np.array([15.9915222, 10.22110037, 10.97796263])),
-    (bifur_sections_two_spheres, np.array([16.339976, 9.35342472, 9.27120319]))
-])
-def test_bifurcation_section_max_size_correct(aorta_centerline_branches ,aorta_surface_branches,
-                                             bifurcation_sections, expectedValue):
-    wrapped_bifur_section = dsa.WrapDataObject(bifurcation_sections(aorta_centerline_branches ,aorta_surface_branches)) 
+@pytest.mark.parametrize("bifurcation_section_type,expectedValue",[
+    ("bifur_sections_one_sphere",  np.array([15.9915222, 10.22110037, 10.97796263])),
+    ("bifur_sections_two_spheres",  np.array([16.339976, 9.35342472, 9.27120319]))
+],
+indirect=["bifurcation_section_type"])
+def test_bifurcation_section_max_size_correct(
+                                              bifurcation_section_type , expectedValue):
+    wrapped_bifur_section = dsa.WrapDataObject(bifurcation_section_type) 
     assert np.allclose(wrapped_bifur_section.CellData.GetArray('BifurcationSectionMaxSize'), expectedValue) == True
 
 
-@pytest.mark.parametrize("bifurcation_sections,expectedValue",[
-    (bifur_sections_one_sphere, np.array([ 0.88737651,  0.89879875,  0.87256458])),
-    (bifur_sections_two_spheres, np.array([ 0.92569699,  0.94823763,  0.96111336]))
-])
-def test_bifurcation_section_shape_correct(aorta_centerline_branches ,aorta_surface_branches,
-                                           bifurcation_sections, expectedValue):
-    wrapped_bifur_section = dsa.WrapDataObject(bifurcation_sections(aorta_centerline_branches ,aorta_surface_branches)) 
+@pytest.mark.parametrize("bifurcation_section_type,expectedValue",[
+    ("bifur_sections_one_sphere",  np.array([ 0.88737651,  0.89879875,  0.87256458])),
+    ("bifur_sections_two_spheres",  np.array([ 0.92569699,  0.94823763,  0.96111336]))
+],
+indirect=["bifurcation_section_type"])
+def test_bifurcation_section_shape_correct(
+                                           bifurcation_section_type , expectedValue):
+    wrapped_bifur_section = dsa.WrapDataObject(bifurcation_section_type) 
     assert np.allclose(wrapped_bifur_section.CellData.GetArray('BifurcationSectionShape'), expectedValue) == True
 
 
-@pytest.mark.parametrize("bifurcation_sections,expectedValue",[
-    (bifur_sections_one_sphere, np.array([1, 1, 0])),
-    (bifur_sections_two_spheres, np.array([1, 1, 1]))
-])
-def test_bifurcation_section_closed_correct(aorta_centerline_branches ,aorta_surface_branches,
-                                           bifurcation_sections, expectedValue):
-    wrapped_bifur_section = dsa.WrapDataObject(bifurcation_sections(aorta_centerline_branches ,aorta_surface_branches)) 
+@pytest.mark.parametrize("bifurcation_section_type,expectedValue",[
+    ("bifur_sections_one_sphere",  np.array([1, 1, 0])),
+    ("bifur_sections_two_spheres",  np.array([1, 1, 1]))
+],
+indirect=["bifurcation_section_type"])
+def test_bifurcation_section_closed_correct(
+                                           bifurcation_section_type , expectedValue):
+    wrapped_bifur_section = dsa.WrapDataObject(bifurcation_section_type) 
     assert np.allclose(wrapped_bifur_section.CellData.GetArray('BifurcationSectionClosed'), expectedValue) == True
 
 
-@pytest.mark.parametrize("bifurcation_sections,expectedValue",[
-    (bifur_sections_one_sphere, np.array([0, 1, 1])),
-    (bifur_sections_two_spheres, np.array([0, 1, 1]))
-])
-def test_bifurcation_section_orientation_correct(aorta_centerline_branches ,aorta_surface_branches,
-                                                 bifurcation_sections, expectedValue):
-    wrapped_bifur_section = dsa.WrapDataObject(bifurcation_sections(aorta_centerline_branches ,aorta_surface_branches)) 
+@pytest.mark.parametrize("bifurcation_section_type,expectedValue",[
+    ("bifur_sections_one_sphere",  np.array([0, 1, 1])),
+    ("bifur_sections_two_spheres",  np.array([0, 1, 1]))
+],
+indirect=["bifurcation_section_type"])
+def test_bifurcation_section_orientation_correct(
+                                                 bifurcation_section_type , expectedValue):
+    wrapped_bifur_section = dsa.WrapDataObject(bifurcation_section_type) 
     assert np.allclose(wrapped_bifur_section.CellData.GetArray('BifurcationSectionOrientation'), expectedValue) == True
 
-    
 
-@pytest.mark.parametrize("bifurcation_sections,expectedValue",[
-    (bifur_sections_one_sphere, np.array([1, 1, 1])),
-    (bifur_sections_two_spheres, np.array([2, 2, 2]))
-])
-def test_bifurcation_section_distance_to_spheres_correct(aorta_centerline_branches ,aorta_surface_branches,
-                                                 bifurcation_sections, expectedValue):
-    wrapped_bifur_section = dsa.WrapDataObject(bifurcation_sections(aorta_centerline_branches ,aorta_surface_branches)) 
+@pytest.mark.parametrize("bifurcation_section_type,expectedValue",[
+    ("bifur_sections_one_sphere",  np.array([1, 1, 1])),
+    ("bifur_sections_two_spheres",  np.array([2, 2, 2]))
+],
+indirect=["bifurcation_section_type"])
+def test_bifurcation_section_distance_to_spheres_correct(
+                                                 bifurcation_section_type, expectedValue):
+    wrapped_bifur_section = dsa.WrapDataObject(bifurcation_section_type) 
     assert np.allclose(wrapped_bifur_section.CellData.GetArray('BifurcationSectionDistanceSpheres'), expectedValue) == True
 
 
