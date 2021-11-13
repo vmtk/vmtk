76171a9285184282013bf24704550fce36e6f945
Kurt Sansom <kayarre@gmail.com>
2021-11-12 23:36:43 -0600

    BUG: Fix vmtkcenterlinesnetwork.py

--- vmtkScripts/vmtkcenterlinesnetwork.py
+++ vmtkScripts/vmtkcenterlinesnetwork.py
@@ -136,7 +136,7 @@ class vmtkCenterlinesNetwork(pypes.pypeScript):
 
         # randomly select one cell to delete so that there is an opening for
         # vmtkNetworkExtraction to use.
-        numCells = self.Surface.GetNumberOfCells()
+        numCells = networkSurface.GetNumberOfCells()
         cellToDelete = random.randrange(0, numCells-1)
         networkSurface.BuildLinks()
         networkSurface.DeleteCell(cellToDelete)
@@ -162,6 +162,7 @@ class vmtkCenterlinesNetwork(pypes.pypeScript):
         nodeIndexToIgnore = np.where(cellDataTopology[:,0] == 0)[0][0]
         keepCellConnectivityList = []
         pointIdxToKeep = np.array([])
+        removeCellLength = 0
         # we remove the cell, points, and point data which are associated with the
         # segment we want to ignore
         for loopIdx, cellConnectivityList in enumerate(ad['CellData']['CellPointIds']):
