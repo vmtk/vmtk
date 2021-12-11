569e6882eb0f8b516bc89de711976074b2ed3924
Kurt Sansom <kayarre@gmail.com>
2021-12-10 20:34:44 -0600

    fix: update boolean test to work with vtk 9.1.0
      fix: update boolean test to work with vtk 9.1.0
      chore: rename for clarity
    
    Updating vmtk to use vtk 9.1.0 revealed output differences in
    vmtksurfacebooleanOperation. Union gives same results. Intersection
    and Difference operators give different results. Some point differs
    on the order 0.01 between the old reference data and vtk 9.1.0 output
    for the intersection and difference tests, versus less than 1.0E-6
    difference in vtk 9.0.3 or earlier.
    
    - Added new tests that use the same criteria as the old test but with new
    reference data and pass as before.
    
    - Added test comparing old reference data using a lower comparison tolerance
    (1e-1) for the difference.
    
    - Used latest Paraview release candidate and boolean filter
    https://discourse.paraview.org/t/boolean-operations/1498/3 to generate the
    new reference data. The test criteria is evaluated on the maximum of the
    range of the distance results, rather than some average difference over the
    whole domain.
    
    see https://github.com/vmtk/vmtk-test-data/pull/3 for updated data

--- tests/test_vmtkScripts/test_vmtksurfacebooleanoperation.py
+++ tests/test_vmtkScripts/test_vmtksurfacebooleanoperation.py
@@ -16,14 +16,15 @@
 
 import pytest
 import vmtk.vmtksurfacebooleanoperation as surfacebooleanoperation
+import vtk
 
 
+@pytest.mark.skipif(vtk.vtkVersion.GetVTKVersion() == '9.1.0', reason="requires vtk version != 9.1.0")
 @pytest.mark.parametrize("operation,paramid", [
-    ('union', '0'),
     ('intersection', '1'),
     ('difference', '2'),
 ])
-def test_operations_default_tolerance(aorta_surface2, aorta_surface_reference,
+def test_operations_default_tolerance_regression(aorta_surface2, aorta_surface_reference,
                                       operation, paramid, compare_surfaces):
     name = __name__ + '_test_operations_default_tolerance_' + paramid + '.vtp'
     booler = surfacebooleanoperation.vmtkSurfaceBooleanOperation()
@@ -33,6 +34,40 @@ def test_operations_default_tolerance(aorta_surface2, aorta_surface_reference,
     booler.Execute()
 
     assert compare_surfaces(booler.Surface, name, tolerance=1E-6) == True
+
+
+@pytest.mark.skipif(vtk.vtkVersion.GetVTKVersion() != '9.1.0', reason="requires vtk == 9.1.0")
+@pytest.mark.parametrize("operation,paramid", [
+    ('intersection', '1'),
+    ('difference', '2'),
+])
+def test_operations_default_tolerance_vtk_v9_1(aorta_surface2, aorta_surface_reference,
+                                      operation, paramid, compare_surfaces):
+    name = __name__ + '_test_operations_default_tolerance_' + paramid + '_vtk-9_1.vtp'
+    booler = surfacebooleanoperation.vmtkSurfaceBooleanOperation()
+    booler.Surface = aorta_surface2
+    booler.Surface2 = aorta_surface_reference
+    booler.Operation = operation
+    booler.Execute()
+
+    assert compare_surfaces(booler.Surface, name, tolerance=1E-6) == True
+
+
+@pytest.mark.parametrize("operation,comp_tol,paramid", [
+    ('union', 1e-6, '0'),
+    ('intersection', 1e-1, '1'),
+    ('difference', 1e-1, '2'),
+])
+def test_operations_default_tolerance(aorta_surface2, aorta_surface_reference,
+                                      operation, comp_tol, paramid, compare_surfaces):
+    name = __name__ + '_test_operations_default_tolerance_' + paramid + '.vtp'
+    booler = surfacebooleanoperation.vmtkSurfaceBooleanOperation()
+    booler.Surface = aorta_surface2
+    booler.Surface2 = aorta_surface_reference
+    booler.Operation = operation
+    booler.Execute()
+
+    assert compare_surfaces(booler.Surface, name, tolerance=comp_tol) == True
 
 
 @pytest.mark.parametrize("operation,tolerance,paramid", [
@@ -50,4 +85,4 @@ def test_operations_varied_tolerance(aorta_surface2, aorta_surface_reference,
     booler.Tolerance = tolerance
     booler.Execute()
 
-    assert compare_surfaces(booler.Surface, name) == True
+    assert compare_surfaces(booler.Surface, name, tolerance=1E-3) == True
